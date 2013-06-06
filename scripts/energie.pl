a4b3244f99d0f0a8ceab8ef096bc5055e42466f6
JeanFred <jeanfred1@gmail.com>
2013-06-06 17:47:06 +0000

    PipelineMiRNA: Refactor energie.pl
    Use warnings and strictures (and update variables)
    Fix evil Open constructs.
    Use File::Spec to build paths.
    Apply DRY.

--- scripts/energie.pl
+++ scripts/energie.pl
@@ -5,41 +5,50 @@
 #                                                                	 #
 #                                                                    #
 ######################################################################
+
+use strict;
+use warnings;
+
+use File::Spec;
+
 my ( $CT, $dirData, $seq ) = @ARGV;
 
 # ouverture du fichier de sortie de Unafold
-open( FOut, $CT ) || die "Problème à l\'ouverture : $!";
-$cg = 0;
-while ( my $line = <FOut> ) {
+open( my $FOut, '<', $CT ) || die "Problème à l\'ouverture : $!";
+
+my $cg = 0;
+my ( $nameSeq, $mfe, $longueur );
+while ( my $line = <$FOut> ) {
 	my @variable = split( '\s+', $line );
-	{
-		if ( @variable[2] =~ /^ENERGY/ ) {
-			$nameSeq = substr @variable[5],
-			  0;    # Récupération du nom de la séquence
-			$mfe      = substr @variable[4], 0;    # Récupération du MFE
-			$longueur = @variable[1];
-			$cg       = 0;
-		}
-		else {
-			if ( @variable[2] =~ /[CG]/ )    # on rencontre un 'g' ou un 'c'
-			{
-				$cg++;
-			}
-			if (   ( $longueur == @variable[1] )
-				&& ( $nameSeq eq $seq )
-			  ) # tester si la longueur actuelle est égale à la longueur de la séquence
-			{
 
-				open( RES, '>> ' . $dirData . '/' . $seq . '/outMFEI.txt' );
-				print RES $nameSeq . "\t"
-				  . ( ( $mfe / $longueur ) * 100 ) /
-				  ( ( $cg / $longueur ) * 100 ) . "\t"
-				  . $mfe . "\t"
-				  . ( $mfe / $longueur ) * 100;
-				system( 'chmod 777 ' . $dirData . '/' . $seq . '/outMFEI.txt' );
-				close RES || die "Problème à la fermeture : $!";
-
-			}
-		}
+	if ( $variable[2] =~ /^ENERGY/ ) {
+		$nameSeq = substr $variable[5],
+		  0;    # Récupération du nom de la séquence
+		$mfe      = substr $variable[4], 0;    # Récupération du MFE
+		$longueur = $variable[1];
+		$cg       = 0;
 	}
-}
+	else {                                     # Not the ENERGY line
+		if ( $variable[2] =~ /[CG]/ )          # on rencontre un 'g' ou un 'c'
+		{
+			$cg++;
+		}
+		if (
+			( $longueur == $variable[1] )
+
+			&& ( $nameSeq eq $seq )
+		  ) # tester si la longueur actuelle est égale à la longueur de la séquence
+		{
+			my $MFEI_output =
+			  File::Spec->catfile( $dirData, $seq, 'outMFEI.txt' );
+            my $num = ( $mfe / $longueur ) * 100;
+            my $other = $num / ( ( $cg / $longueur ) * 100 );
+			open( my $RES, '>>', $MFEI_output );
+			print $RES $nameSeq . "\t" . $other . "\t" . $mfe . "\t" . $num;
+			close $RES || die "Problème à la fermeture : $!";
+	        system( 'chmod 777 ' . $MFEI_output );
+		}
+
+	}    # /Else− not ENERGY line
+}    # /While
+close $FOut || die "Problème à la fermeture : $!";
