5c3169b9ff41f822cd7af2c20075bb878b2b6560
JeanFred <jeanfred1@gmail.com>
2013-06-17 16:57:49 +0000

    PipelineMiRNA: Tidying energie.pl

--- scripts/energie.pl
+++ scripts/energie.pl
@@ -19,46 +19,47 @@ open( my $FOut, '<', $CT ) || die "Unable to open: $!";
 my $cg = 0;
 my ( $nameSeq, $mfe, $longueur );
 while ( my $line = <$FOut> ) {
-	my @variable = split( '\s+', $line );
+    my @variable = split( '\s+', $line );
 
-	if (
-		$variable[2] =~ m{
+    if (
+        $variable[2] =~ m{
 		                  ^              # Begin of line
 		                  ENERGY         # ENERGY
 		                }smx
-	  )
-	{
-		$nameSeq = substr $variable[5],
-		  0;    # Récupération du nom de la séquence
-		$mfe      = substr $variable[4], 0;    # Récupération du MFE
-		$longueur = $variable[1];
-		$cg       = 0;
-	}
-	else {                                     # Not the ENERGY line
-		if (
-			$variable[2] =~ m{
+      )
+    {
+        $nameSeq = substr $variable[5],
+          0;    # Récupération du nom de la séquence
+        $mfe      = substr $variable[4], 0;    # Récupération du MFE
+        $longueur = $variable[1];
+        $cg       = 0;
+    }
+    else {                                     # Not the ENERGY line
+        if (
+            $variable[2] =~ m{
 			                  [CG]     # G or C
 		                    }smxi
-		  )
-		{
-			$cg++;
-		}
-		if (
-			( $longueur == $variable[1] )
+          )
+        {
+            $cg++;
+        }
+        if (
+            ( $longueur == $variable[1] )
 
-			&& ( $nameSeq eq $seq )
-		  ) # tester si la longueur actuelle est égale à la longueur de la séquence
-		{
-			my $MFEI_output =
-			  File::Spec->catfile( $dirData, $seq, 'outMFEI.txt' );
-			my $num = ( $mfe / $longueur ) * 100;
-			my $other = $num / ( ( $cg / $longueur ) * 100 );
-			open( my $RES, '>>', $MFEI_output ) || die "Unable to open: $!";
-			print $RES $nameSeq . "\t" . $other . "\t" . $mfe . "\t" . $num;
-			close $RES || die "Unable to close: $!";
-			system( 'chmod 777 ' . $MFEI_output );
-		}
+            && ( $nameSeq eq $seq )
+          ) # tester si la longueur actuelle est égale à la longueur de la séquence
+        {
+            my $MFEI_output =
+              File::Spec->catfile( $dirData, $seq, 'outMFEI.txt' );
+            my $num = ( $mfe / $longueur ) * 100;
+            my $other = $num / ( ( $cg / $longueur ) * 100 );
+            open( my $RES, '>>', $MFEI_output ) || die "Unable to open: $!";
+            my $content = $nameSeq . "\t" . $other . "\t" . $mfe . "\t" . $num;
+            print $RES $content;
+            close $RES || die "Unable to close: $!";
+            system( 'chmod 777 ' . $MFEI_output );
+        }
 
-	}    # /Else− not ENERGY line
+    }    # /Else− not ENERGY line
 }    # /While
 close $FOut || die "Unable to close: $!";
