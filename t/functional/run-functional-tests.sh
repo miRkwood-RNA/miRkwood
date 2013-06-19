eaebe7bea841ca1da2ed3e8d722e5aae262ba389
JeanFred <jeanfred1@gmail.com>
2013-06-19 18:40:03 +0000

    PipelineMiRNA: Add functional test for FilterCDS
    -Add functional test to shell script
    -Add input data
    -Add expected output

--- t/functional/run-functional-tests.sh
+++ t/functional/run-functional-tests.sh
@@ -19,9 +19,12 @@ BASEDIR=$(dirname $SCRIPT)
 ROOTDIR=$BASEDIR/../..
 
 # Testing
-plan 1
+plan 2
 
 ### Testing script of the PipelineMiRNA
 rm -rf $BASEDIR/output/sequenceSomething/ && mkdir -p $BASEDIR/output/sequenceSomething/ && cp $BASEDIR/data/sequenceSomething.fas $BASEDIR/output/sequenceSomething/sequenceUpload.fas
-perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl unChecked  mfeiChecked randfoldChecked UNSCChecked UNalignChecked $BASEDIR/output/sequenceSomething/
-ok 'First test' [ `diff --exclude=pvalue.txt -qr $BASEDIR/output/sequenceSomething/ $BASEDIR/expected/sequenceSomething/ | wc -l` -lt 1 ]
+perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl unChecked  mfeiChecked randfoldChecked UNSCChecked UNalignChecked $BASEDIR/output/sequenceSomething/ ATpepTAIR10
+ok 'Full pipeline' [ `diff --exclude=pvalue.txt -qr $BASEDIR/output/sequenceSomething/ $BASEDIR/expected/sequenceSomething/ | wc -l` -eq 0 ]
+rm -rf $BASEDIR/output/filtercds/ && mkdir -p $BASEDIR/output/filtercds/ && cp $BASEDIR/data/filtercds_in.fas $BASEDIR/output/filtercds/sequenceUpload.fas
+perl -I$ROOTDIR/lib $ROOTDIR/scripts/filterCDS.pl $ROOTDIR/programs/ncbi-blast-2.2.28+-src/c++/GCC460-Debug/bin $ROOTDIR/data/ $BASEDIR/output/filtercds/ ATpepTAIR10
+ok 'FilterCDS' [ `diff -qr $BASEDIR/expected/filtercds/ $BASEDIR/output/filtercds/ | wc -l` -eq 0 ]
