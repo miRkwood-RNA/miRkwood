cb9fda54b2649ff6357959af593dd92ed55dd58b
JeanFred <jeanfred1@gmail.com>
2013-06-20 14:17:47 +0000

    PipelineMiRNA: Tidying and formatting FilterCDS

--- scripts/filterCDS.pl
+++ scripts/filterCDS.pl
@@ -15,7 +15,7 @@ use PipelineMiRNA::Utils;
 my ( $idirData, $idirJob, $iplant ) = @ARGV;
 main( $idirData, $idirJob, $iplant );
 
-sub main {
+sub filter_CDS {
     my ( $dirData, $dirJob, $plant ) = @_;
 
     my $uploaded_sequences = File::Spec->catfile( $dirJob, 'sequenceUpload.fas' );
@@ -27,8 +27,8 @@ sub main {
     PipelineMiRNA::Programs::run_blast($uploaded_sequences,
                                        $blast_database,
                                        $blastx_options,
-                                       $blast_output)
-      or die('Problem when running Blastx');
+                                       $blast_output
+    ) or die('Problem when running Blastx');
     chmod 777, $blast_output;
 
     my %blast_seqs;
@@ -38,18 +38,18 @@ sub main {
       || die "Problème à l\'ouverture : $!";
     while ( my $line = <$FOut> ) {
         my @name = split( '\t', $line );
-        $blast_seqs{$name[0]} = 1;
+        $blast_seqs{ $name[0] } = 1;
     }
     close $FOut || die "Problème à la fermeture : $!";
 
     # Looping in uploaded_sequences, copying only the files not found in Blast
     open( my $FSeq, '<', $uploaded_sequences )
       or die "Problème à l\'ouverture : $!";
-    open( my $RES_FH,  '>>', $input_sequences )
+    open( my $RES_FH, '>>', $input_sequences )
       or die "Problème à l\'ouverture : $!";
-    PipelineMiRNA::Utils::filter_fasta($FSeq, $RES_FH, \%blast_seqs);
-    close $FSeq or die "Problème à la fermeture : $!";
-    close $RES_FH  or die "Problème à la fermeture : $!";
+    PipelineMiRNA::Utils::filter_fasta( $FSeq, $RES_FH, \%blast_seqs );
+    close $FSeq   or die "Problème à la fermeture : $!";
+    close $RES_FH or die "Problème à la fermeture : $!";
     chmod 777, $input_sequences;
 
     return $input_sequences;
