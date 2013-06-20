384090b7c17fab9531ca925e31decc0be45ef0ea
JeanFred <jeanfred1@gmail.com>
2013-06-20 14:17:55 +0000

    PipelineMiRNA: Add functional tests for Filtering CDS

--- t/functional/run-functional-tests.sh
+++ t/functional/run-functional-tests.sh
@@ -19,12 +19,17 @@ BASEDIR=$(dirname $SCRIPT)
 ROOTDIR=$BASEDIR/../..
 
 # Testing
-plan 2
+plan 3
 
 ### Testing script of the PipelineMiRNA
-rm -rf $BASEDIR/output/sequenceSomething/ && mkdir -p $BASEDIR/output/sequenceSomething/ && cp $BASEDIR/data/sequenceSomething.fas $BASEDIR/output/sequenceSomething/sequenceUpload.fas
-perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl unChecked  mfeiChecked randfoldChecked UNSCChecked UNalignChecked $BASEDIR/output/sequenceSomething/ ATpepTAIR10
-ok 'Full pipeline' [ `diff --exclude=pvalue.txt -qr $BASEDIR/output/sequenceSomething/ $BASEDIR/expected/sequenceSomething/ | wc -l` -eq 0 ]
+rm -rf $BASEDIR/output/fullpipeline1/ && mkdir -p $BASEDIR/output/fullpipeline1/ && cp $BASEDIR/data/sequenceSomething.fas $BASEDIR/output/fullpipeline1/sequenceUpload.fas
+perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl unChecked  mfeiChecked randfoldChecked UNSCChecked UNalignChecked $BASEDIR/output/fullpipeline1/ ATpepTAIR10
+ok 'Full pipeline' [ `diff --exclude=pvalue.txt -qr $BASEDIR/output/fullpipeline1/ $BASEDIR/expected/fullpipeline1/ | wc -l` -eq 0 ]
+
+rm -rf $BASEDIR/output/fullpipeline2/ && mkdir -p $BASEDIR/output/fullpipeline2/ && cp $BASEDIR/data/filtercds_in.fas $BASEDIR/output/fullpipeline2/sequenceUpload.fas
+perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl checked  mfeiChecked randfoldChecked UNSCChecked UNalignChecked $BASEDIR/output/fullpipeline2/ ATpepTAIR10
+ok 'Full pipeline with FilteringCDS' [ `diff --exclude=pvalue.txt -qr $BASEDIR/output/fullpipeline2/ $BASEDIR/expected/fullpipeline2/ | wc -l` -eq 0 ]
+
 rm -rf $BASEDIR/output/filtercds/ && mkdir -p $BASEDIR/output/filtercds/ && cp $BASEDIR/data/filtercds_in.fas $BASEDIR/output/filtercds/sequenceUpload.fas
 perl -I$ROOTDIR/lib $ROOTDIR/scripts/filterCDS.pl $ROOTDIR/data/ $BASEDIR/output/filtercds/ ATpepTAIR10
 ok 'FilterCDS' [ `diff -qr $BASEDIR/expected/filtercds/ $BASEDIR/output/filtercds/ | wc -l` -eq 0 ]
