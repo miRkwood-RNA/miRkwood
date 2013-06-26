24a4d7a5faae3b01a4f833b738c3f63ae284c8a2
JeanFred <jeanfred1@gmail.com>
2013-06-26 17:33:02 +0000

    PipelineMiRNA: Revamp & tweak results.pl

--- cgi-bin/results.pl
+++ cgi-bin/results.pl
@@ -1,128 +1,171 @@
 #!/usr/bin/perl -w
 use Class::Struct;
-use CGI; 
+use CGI;
 use Time::gmtime;
 use File::Spec;
 use FindBin qw($Bin);
 use Cwd qw( abs_path );
 use File::Basename qw(dirname);
+use CGI::Carp qw(fatalsToBrowser);
+use Log::Message::Simple qw[msg error debug];
 
-my $cgi = new CGI; 
+my $cgi = new CGI;
 $now = gmctime();
 
 $now =~ s/[: ]//g;
-$now = substr($now, 3);
-my $dirJob_name = 'job'.$now;
+$now = substr( $now, 3 );
 
 my $local_dir = dirname( abs_path($0) );
 my $rootdir = File::Spec->catdir( $local_dir, ".." );
 
-$dirScript = File::Spec->catdir( $rootdir, 'scripts');
-$dirData = File::Spec->catdir( $rootdir, 'data');
-$dirLib = File::Spec->catdir( $rootdir, 'lib');
+$dirScript = File::Spec->catdir( $rootdir, 'scripts' );    # chemin script
 
-my $formatFasta_bin = File::Spec->catfile($dirScript, 'formatFasta.sh');
+#$dirScript = "/var/www/arn/scripts/"; # chemin script
 
-$dirJob = File::Spec->catdir( $dirData, $dirJob_name); # chemin séquence de base
+$dirData   = File::Spec->catdir( $rootdir, 'data' );
+$dirLib    = File::Spec->catdir( $rootdir, 'lib' );
+$dirImages = File::Spec->catdir( $rootdir, 'images' );
+
+my $formatFasta_bin = File::Spec->catfile( $dirScript, 'formatFasta.sh' );
+
+$dirJob_name = 'job' . $now;
+$dirJob = File::Spec->catdir( $rootdir, 'results', $dirJob_name )
+  ;    # chemin séquence de base
+
+#$dirJob = $dirData."job".$now."/"; # chemin séquence de base
 mkdir $dirJob;
 
-my $sequence = File::Spec->catfile($dirJob, 'sequence.fas');
-my $sequence_load = File::Spec->catfile($dirJob, 'sequenceLoad.fas');
-my $sequence_upload = File::Spec->catfile($dirJob, 'sequenceUpload.fas');
+my $log_file = File::Spec->catfile( $dirJob, 'log.log' );
+open( my $LOG, '>>', $log_file ) || die "$!";
+
+my $sequence_origin = File::Spec->catfile( $dirJob, 'sequence.fas' );
+my $sequence_load   = File::Spec->catfile( $dirJob, 'sequenceLoad.fas' );
+my $sequence_upload = File::Spec->catfile( $dirJob, 'sequenceUpload.fas' );
 
 my $seqArea = $cgi->param('seqArea');
-if ($seqArea eq "")  # cas upload fichier
-{	
-	$seq = "";
-	my $upload = $cgi->upload('seqFile') || die "$!";
-	open (INPUT, '>>', $sequence) || die "$!";
-	while (my $ligne = <$upload>) 
-	{
-		$seq.= $ligne;
-	}
-	print INPUT lc($seq)."\n"; ##mise en minuscules
-	close INPUT;
-	chmod 777, $sequences;
-	$seq = lc($seq)."\n";
-	my $formatFasta_cmd = "sh $formatFasta_bin $sequence $sequence_load";
-	system($formatFasta_cmd) ; # script qui elimine les retour a la ligne 
-	if ($seq !~ /^( *>.+[\r\n]+([-\. atcgunwkmsydr0-9]+[\r\n]+)+){1,}$/)  
-	{# erreur de syntaxe
-		print $cgi->redirect('http://'.$ENV{SERVER_NAME}.'/cgi-bin/error.pl');       exit; 
-	}
-}
-else #cas textArea
+if ( $seqArea eq "" )    # cas upload fichier
 {
-	open (INPUT, '>>', $sequences) || die "$!";
-	print INPUT lc($seqArea);
-	system ("sed -i 'N;s/\r//g' ".$sequences); #commande sed permettant d'eliminer les caracteres du (copier-coller)
-	system("sh $formatFasta_bin $sequence $sequence_load" ) ; # script qui elimine les retour a la ligne 
-	while (my $ligne = <INPUT>) 
-	{
-		$seqArea.= $ligne;
-	}
-	$seqArea = lc($seqArea)."\n";#mise en miniscule
-	
-	if ($seqArea !~ /^( *>.+[\r\n]+([-\. atcgunwkmsydr0-9]+[\r\n]+)+){1,}$/)  
-	{# erreur de syntaxe
-		print $cgi->redirect('http://'.$ENV{SERVER_NAME}.'/cgi-bin/error.pl');       exit; 
-	}
-	close INPUT;
+    $seq = "";
+    my $upload = $cgi->upload('seqFile')
+      || die "Error when getting seqFile: $!";
+    while ( my $ligne = <$upload> ) {
+        $seq .= $ligne;
+    }
+    open( INPUT, '>>', $sequence_origin )
+      || die "Error when opening -$sequence_origin-: $!";
+    print INPUT lc($seq) . "\n";    ##mise en minuscules
+    close INPUT;
+    chmod 777, $sequence_origin;
+    $seq = lc($seq) . "\n";
+    print $LOG "Captured $seq";
+    my $fasta_cmd = "sh $formatFasta_bin $sequence_origin $sequence_load";
+    print $LOG "Run $fasta_cmd";
+    system($fasta_cmd);             # script qui elimine les retour a la ligne
+
+    if ( $seq !~ /^( *>.+[\r\n]+([-\. atcgunwkmsydr0-9]+[\r\n]+)+){1,}$/ )
+    {                               # erreur de syntaxe
+        print $cgi->redirect(
+            'http://' . $ENV{SERVER_NAME} . '/cgi-bin/error.pl' );
+        exit;
+    }
+}
+else                                #cas textArea
+{
+    open( INPUT, '>>', $sequence_origin )
+      || die "Error when opening -$sequence_origin-: $!";
+    print INPUT lc($seqArea);
+    system( "sed -i 'N;s/\r//g' " . $sequence_origin )
+      ;    #commande sed permettant d'eliminer les caracteres du (copier-coller)
+    system("sh $formatFasta_bin $sequence_origin $sequence_load")
+      ;    # script qui elimine les retour a la ligne
+    while ( my $ligne = <INPUT> ) {
+        $seqArea .= $ligne;
+    }
+    $seqArea = lc($seqArea) . "\n";    #mise en miniscule
+
+    if ( $seqArea !~ /^( *>.+[\r\n]+([-\. atcgunwkmsydr0-9]+[\r\n]+)+){1,}$/ )
+    {                                  # erreur de syntaxe
+        print $cgi->redirect(
+            'http://' . $ENV{SERVER_NAME} . '/cgi-bin/error.pl' );
+        exit;
+    }
+    close INPUT;
 }
 
-	open (LOAD, $sequence_load) || die "$!";
-	open (OUTPUT, '>>', $sequence_upload) || die "$!";
-	while (my $line = <LOAD>) 
-	{
-		if ($line=~/^>/)
-		{
-			$name = $line ; 
-		}
-		else
-		{
-			$line=~s/w/n/g;
-			$line=~s/r/n/g;
-			$line=~s/k/n/g;
-			$line=~s/m/n/g;
-			$line=~s/s/n/g;
-			$line=~s/y/n/g;
-			$line=~s/d/n/g;
-			print OUTPUT $name.$line;
-			$name = "";
-		}
-	
-	}
-	unlink ($sequence_load);
+open( LOAD, '<', $sequence_load )
+  or die "Error when opening -$sequence_load-: $!";
+open( OUTPUT, '>>', $sequence_upload )
+  or die "Error when opening -$sequence_upload-: $!";
+while ( my $line = <LOAD> ) {
+    if ( $line =~ /^>/ ) {
+        $name = $line;
+    }
+    else {
+        $line =~ s/w/n/g;
+        $line =~ s/r/n/g;
+        $line =~ s/k/n/g;
+        $line =~ s/m/n/g;
+        $line =~ s/s/n/g;
+        $line =~ s/y/n/g;
+        $line =~ s/d/n/g;
+        print OUTPUT $name . $line;
+        $name = "";
+    }
+}
+unlink($sequence_load);
 
 # redirection vers la page wait en attendant le calcul
 my $mail = $cgi->param('mail');
 
 my $nameJob = $cgi->param('job');
 
-print $cgi->redirect(-uri=>'http://'.$ENV{SERVER_NAME}.'/cgi-bin/wait.pl?jobId='.$now.'&mail='.$mail.'&nameJob='.$nameJob);
+print $cgi->redirect( -uri => 'http://'
+      . $ENV{SERVER_NAME}
+      . '/cgi-bin/wait.pl?jobId='
+      . $now
+      . '&mail='
+      . $mail
+      . '&nameJob='
+      . $nameJob );
 
+print $cgi->redirect( -uri => 'http://'
+      . $ENV{SERVER_NAME}
+      . '/cgi-bin/wait.pl?jobId='
+      . $now
+      . '&mail='
+      . $mail );
+print "Location: http://"
+  . $ENV{SERVER_NAME}
+  . "wait.pl?jobId="
+  . $now
+  . "&mail="
+  . $mail . "\n\n";
 
-
-print $cgi->redirect(-uri=>'http://'.$ENV{SERVER_NAME}.'/cgi-bin/wait.pl?jobId='.$now.'&mail='.$mail);
-print "Location: http://".$ENV{SERVER_NAME}."wait.pl?jobId=".$now."&mail=".$mail."\n\n";
-
-my $check = $cgi->param('check');
-my $mfei = $cgi->param('mfei');
+my $check    = $cgi->param('check');
+my $mfei     = $cgi->param('mfei');
 my $randfold = $cgi->param('randfold');
-my $SC = $cgi->param('selfContain');
-my $align = $cgi->param('align');
-$plant =  $cgi->param('db');
-if ($mfei eq "") { $mfei = "notChecked" };
-if ($randfold eq "") { $randfold = "notChecked" };
-if ($SC eq "") { $SC = "notChecked" };
-if ($align eq "") { $align = "notChecked" };
-if ($check eq "") { $check = "notChecked" };
+my $SC       = $cgi->param('selfContain');
+my $align    = $cgi->param('align');
+my $plant    = $cgi->param('db');
+if ( !$plant ) {
+    $plant = "NonePlant";
+}
+if ( $mfei     eq "" ) { $mfei     = "notChecked" }
+if ( $randfold eq "" ) { $randfold = "notChecked" }
+if ( $SC       eq "" ) { $SC       = "notChecked" }
+if ( $align    eq "" ) { $align    = "notChecked" }
+if ( $check    eq "" ) { $check    = "notChecked" }
+
 #execution de tous les scripts de traitements
-my $perl_script = File::Spec->catfile($dirScript, 'execute_scripts.pl');
-my $cmd = "perl -I$dirLib $perl_script $check $mfei $randfold $SC $align $dirJob $plant";
+my $perl_script = File::Spec->catfile( $dirScript, 'execute_scripts.pl' );
+my $cmd =
+"perl -I$dirLib $perl_script $check $mfei $randfold $SC $align $dirJob $plant";
+
+print $LOG "The command is: $cmd";
+
 system($cmd);
 
-my $finish_file = File::Spec->catfile($dirJob, 'finished');
-open (my $finish, '>', $finish_file) || die "$!";
+my $finish_file = File::Spec->catfile( $dirJob, 'finished' );
+open( my $finish, '>', $finish_file ) || die "$!";
 close $finish_file;
-
+close $log_file;
