a353dbd8ab7fca2cc36daa87bd5e0a27242ef5ea
JeanFred <jeanfred1@gmail.com>
2013-07-09 17:39:57 +0000

    PipelineMiRNA: Tidying, fixing critic issues

--- lib/PipelineMiRNA/Utils.pm
+++ lib/PipelineMiRNA/Utils.pm
@@ -23,10 +23,10 @@ sub parse_multi_fasta {
 }
 
 sub filter_fasta {
-    my $FASTA_FH = shift;
-    my $RES_FH = shift;
+    my $FASTA_FH           = shift;
+    my $RES_FH             = shift;
     my $sequence_to_filter = shift;
-    my %sequence_to_filter = %$sequence_to_filter;
+    my %sequence_to_filter = %{$sequence_to_filter};
 
     my $lineSeq;
     my $RESULT = "";
@@ -34,31 +34,38 @@ sub filter_fasta {
         if ( grep { /^>/msx } $line ) {
             $lineSeq = substr $line, 1, -1;
         }
-        if(!exists($sequence_to_filter{$lineSeq})) {
+        if ( !exists( $sequence_to_filter{$lineSeq} ) ) {
             printf $RES_FH $line;
         }
     }
     return;
 }
 
+=method find_matching_count
+
+Get the number of matchings in a stemloop.
+
+=cut
+
 sub find_matching_count {
-    my $structure = shift;
-    my $stop = 1;
+    my $structure          = shift;
+    my $stop               = 1;
     my $parenthesisCounter = 0;
-    my $iteration = -1;
+    my $iteration          = -1;
     my $element;
-    while($stop){
+    while ($stop) {
         $iteration += 1;
-        $element = substr($structure, $iteration, 1);
-        if($element eq '.'){
+        $element = substr( $structure, $iteration, 1 );
+        if ( $element eq '.' ) {
         }
-        elsif($element eq '('){
+        elsif ( $element eq '(' ) {
             $parenthesisCounter += 1;
-        }elsif($element eq ')'){
+        }
+        elsif ( $element eq ')' ) {
             $stop = 0;
         }
     }
-    return $parenthesisCounter
+    return $parenthesisCounter;
 }
 
 =method make_loop
@@ -69,32 +76,29 @@ Return an ASCII representation of the final loop.
 
 sub make_loop {
     my $sequence = shift;
-    my (@top, @upper, @middle, @lower, @bottom);
-    my $quotient = int( (length($sequence) - 2) / 2);
-    print $sequence, " ", length($sequence), " ", $quotient;
-    my $modulo = int(length($sequence) % 2);
-    push(@middle, (" ") x $quotient);
-    push(@upper, (" ") x $quotient);
-    push(@lower, (" ") x $quotient);
-    if ($modulo != 0){
-        push(@middle, substr($sequence, $quotient + 1, 1));
+    my $SPACE    = q{ };
+    my ( @top, @upper, @middle, @lower, @bottom );
+    my $quotient = int( ( length($sequence) - 2 ) / 2 );
+    print $sequence, $SPACE, length($sequence), $SPACE, $quotient;
+    my $modulo = int( length($sequence) % 2 );
+    push( @middle, ($SPACE) x $quotient );
+    push( @upper,  ($SPACE) x $quotient );
+    push( @lower,  ($SPACE) x $quotient );
+    if ( $modulo != 0 ) {
+        push( @middle, substr( $sequence, $quotient + 1, 1 ) );
     }
-    push(@top, split('', substr($sequence, 0, $quotient)));
-    push(@upper, split('', substr($sequence, $quotient, 1)));
-    push(@lower, split('', substr($sequence, $quotient + 2, 1)));
-    push(@bottom, split('', substr($sequence, $quotient + 2, $quotient)));
+    push( @top,    split( '', substr( $sequence, 0,             $quotient ) ) );
+    push( @upper,  split( '', substr( $sequence, $quotient,     1 ) ) );
+    push( @lower,  split( '', substr( $sequence, $quotient + 2, 1 ) ) );
+    push( @bottom, split( '', substr( $sequence, $quotient + 2, $quotient ) ) );
     my @AOA;
-    $AOA[0] = [ @top ];
-    $AOA[1] = [ @upper ];
-    $AOA[2] = [ @middle ];
-    $AOA[3] = [ @lower ];
-    $AOA[4] = [ @bottom ];
-    print "\n============\n";
-    print @upper;
-    print "\n============\n";
+    $AOA[0] = [@top];
+    $AOA[1] = [@upper];
+    $AOA[2] = [@middle];
+    $AOA[3] = [@lower];
+    $AOA[4] = [@bottom];
     return @AOA;
 }
-
 
 =method make_ASCII_viz
 
@@ -103,79 +107,85 @@ Return an ASCII representation of a stemloop.
 =cut
 
 sub make_ASCII_viz {
-    print "make_ASCII_viz()\n";
-    my ($sequence, $structure) = @_;
+    my ( $sequence, $structure ) = @_;
 
     # Variables initialisation
+    my $SPACE = q{ };
     my @top;
     my @upper;
     my @middle;
     my @lower;
     my @bottom;
-    my $left = 0;
-    my $right = length($sequence) - 1;
+    my $left                = 0;
+    my $right               = length($sequence) - 1;
     my $parenthesis_counter = 0;
-    my $stop = 0;
-    my $parenthesis_number = find_matching_count($structure);
+    my $stop                = 0;
+    my $parenthesis_number  = find_matching_count($structure);
 
-    while (! $stop){
-        my $element_left = substr($structure, $left, 1);
-        my $letter_left = substr($sequence, $left, 1);
-        my $element_right = substr( $structure, $right, 1);
-        my $letter_right = substr( $sequence, $right, 1);
+    while ( !$stop ) {
+        my $element_left  = substr( $structure, $left,  1 );
+        my $letter_left   = substr( $sequence,  $left,  1 );
+        my $element_right = substr( $structure, $right, 1 );
+        my $letter_right  = substr( $sequence,  $right, 1 );
 
-        if ($element_left eq '.' and $element_right eq ')'){
-            push(@top, $letter_left);
-            push(@upper, ' ');
-            push(@middle, ' ');
-            push(@lower, ' ');
-            push(@bottom, '-');
+        if ( $element_left eq '.' and $element_right eq ')' ) {
+            push( @top,    $letter_left );
+            push( @upper,  $SPACE );
+            push( @middle, $SPACE );
+            push( @lower,  $SPACE );
+            push( @bottom, '-' );
             $left += 1;
-        }elsif( $element_left eq "(" and $element_right eq ')'){
-            push(@top, ' ');
-            push(@upper, $letter_left);
-            push(@middle, '|');
-            push(@lower, $letter_right);
-            push(@bottom, ' ');
+        }
+        elsif ( $element_left eq '(' and $element_right eq ')' ) {
+            push( @top,    $SPACE );
+            push( @upper,  $letter_left );
+            push( @middle, '|' );
+            push( @lower,  $letter_right );
+            push( @bottom, $SPACE );
             $left += 1;
             $right -= 1;
             $parenthesis_counter += 1;
-        }elsif( $element_left eq "(" and $element_right eq '.'){
-            push(@top, '-');
-            push(@upper, ' ');
-            push(@middle, ' ');
-            push(@lower, ' ');
-            push(@bottom, $letter_right);
+        }
+        elsif ( $element_left eq '(' and $element_right eq '.' ) {
+            push( @top,    '-' );
+            push( @upper,  $SPACE );
+            push( @middle, $SPACE );
+            push( @lower,  $SPACE );
+            push( @bottom, $letter_right );
             $right -= 1;
-        }elsif( $element_left eq "." and $element_right eq '.'){
-            push(@top, $letter_left);
-            push(@upper, ' ');
-            push(@middle, ' ');
-            push(@lower, ' ');
-            push(@bottom, $letter_right);
+        }
+        elsif ( $element_left eq '.' and $element_right eq '.' ) {
+            push( @top,    $letter_left );
+            push( @upper,  $SPACE );
+            push( @middle, $SPACE );
+            push( @lower,  $SPACE );
+            push( @bottom, $letter_right );
             $left += 1;
             $right -= 1;
-        }else{
+        }
+        else {
             $stop = 1;
         }
 
-        if ($left ge $parenthesis_number
-            and length($sequence) - $right - 1 ge $parenthesis_number){
+        if ( $left ge $parenthesis_number
+            and length($sequence) - $right - 1 ge $parenthesis_number )
+        {
             $stop = 1;
         }
 
     }
 
-    my $subsequence = substr($sequence, $left, $right - $left - 1);
+    my $subsequence = substr( $sequence, $left, $right - $left - 1 );
     my @final_loop = make_loop($subsequence);
-#    @{$hit}[1]
-    my @top_end =  @{${final_loop[0]}};
-    my @upper_end =  @{${final_loop[1]}};
-    my @middle_end =  @{${final_loop[2]}};
-    my @lower_end =  @{${final_loop[3]}};
-    my @bottom_end =  @{${final_loop[4]}};
 
-    my $result = <<END;
+    #    @{$hit}[1]
+    my @top_end    = @{ ${ final_loop [0] } };
+    my @upper_end  = @{ ${ final_loop [1] } };
+    my @middle_end = @{ ${ final_loop [2] } };
+    my @lower_end  = @{ ${ final_loop [3] } };
+    my @bottom_end = @{ ${ final_loop [4] } };
+
+    my $result = <<"END";
 @top @top_end
 @upper @upper_end
 @middle @middle_end
@@ -186,4 +196,3 @@ END
 }
 
 1;
-
