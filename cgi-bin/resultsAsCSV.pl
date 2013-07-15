02963fd5d9d33520bdc3a73c9306c7610030fbe2
JeanFred <jeanfred1@gmail.com>
2013-07-15 16:57:54 +0000

    PipelineMiRNA: Revamp CSV export
    We changed basically everything in a previous commit, including
    the general flow and the results data structure.
    The new CGI script version is changed accordingly.

--- cgi-bin/resultsAsCSV.pl
+++ cgi-bin/resultsAsCSV.pl
@@ -16,18 +16,24 @@ use PipelineMiRNA::WebFunctions;
 my $local_dir = dirname( abs_path($0) );
 my $rootdir = File::Spec->catdir( $local_dir, '..' );
 
-my $id_job      = $cgi->param('run_id');    # récupération id job
-my $dirJob_name = 'job' . $id_job;
-my $dirJob =
-  abs_path( File::Spec->catdir( $rootdir, 'results', $dirJob_name ) );
+my $id_job = $cgi->param('run_id');    # récupération id job
 
-my $csv_file = File::Spec->catfile( $dirJob, 'result.csv' );
-open( my $CSV, '<', $csv_file ) or die "Error when opening $csv_file: $!";
-my @csv = <$CSV>;
-close $CSV or die "Cannot close $csv_file: $!";
-print <<"DATA" or die "Error when printing content: $!";
+my $valid = PipelineMiRNA::WebFunctions->is_valid_jobID($id_job);
+
+if ($valid) {
+    my %myResults =
+      PipelineMiRNA::WebFunctions->get_structure_for_jobID($id_job);
+    my $HTML_results =
+      PipelineMiRNA::WebFunctions->resultstruct2pseudoXML( \%myResults );
+    my $csv = PipelineMiRNA::WebFunctions->resultstruct2csv( \%myResults );
+
+    print <<"DATA" or die "Error when printing content: $!";
 Content-type: text/csv
-Content-disposition: attachment;filename=Results.csv
+Content-disposition: attachment;filename=Results-$id_job.csv
 
-@csv
+$csv
 DATA
+}
+else {
+    die('Error');
+}
