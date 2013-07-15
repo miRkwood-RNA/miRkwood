a19959276bcb8745e261b3f5adb91f4819f99fa5
JeanFred <jeanfred1@gmail.com>
2013-07-15 16:57:53 +0000

    PipelineMiRNA: Modularize resultsWithID in WebFunctions
    All methods and, basically, code, is moved to module WebFunctions.
    Various methods are revamped.

--- cgi-bin/resultsWithID.pl
+++ cgi-bin/resultsWithID.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl -w
+use strict;
+use warnings;
+
 use Class::Struct;
 use CGI;
 my $cgi = CGI->new;
@@ -6,239 +9,59 @@ use CGI::Carp qw(fatalsToBrowser);
 use Cwd qw( abs_path );
 use File::Basename qw(dirname);
 use File::Spec;
-
+use Data::Dumper;
 use FindBin;                     # locate this script
 use lib "$FindBin::Bin/../lib";  # use the parent directory
 use PipelineMiRNA::WebFunctions;
 
 
-my $local_dir = dirname( abs_path($0) );
-my $rootdir = File::Spec->catdir( $local_dir, ".." );
+my $id_job = $cgi->param('run_id'); # récupération id job
+my $name_job = $cgi->param('nameJob'); # récupération id job
 
-$id_job = $cgi->param('run_id'); # récupération id job
-my $dirJob_name = 'job'.$id_job;
-
-$dirJob = abs_path(File::Spec->catdir( $rootdir, 'results', $dirJob_name));
-
-$names =[]; 
-$pvalues =[]; 
-$positions =[]; 
-$mfeis =[]; 
-$mfes =[];
-$amfes =[];
-$namesPositions =[]; 
-$DNASequence =[];
-$Vienna =[];  
-$alignement =[];
-$selfContain =[];
-struct results => {        # déclaration de la structure de données 
-	names  => '@',         
-	pvalues => '@',
-	positions => '@',
-	mfeis => '@',
-	mfes => '@',   
-	amfes => '@',
-	namesPositions=> '@',
-	DNASequence=> '@',
-	Vienna=> '@', 
-	alignement=> '@',
-	selfContain=> '@',
-};
-#$name_job = $cgi->param('nameJob'); # récupération id job	
-
-opendir DIR, $dirJob; #ouverture répertoire job
-my @dirs;
-@dirs=readdir DIR;
-closedir DIR;
-foreach $dir(@dirs) # parcours du contenu
-{
-    $full_dir = File::Spec->catdir( $dirJob, $dir );
-	if ($dir ne "." && $dir ne ".." && -d $full_dir) #si fichier est un répertoire
-	{
-		opendir DIR, $full_dir; # ouverture du sous répertoire
-		my @files;
-		@files=readdir DIR;
-		closedir DIR;
-		foreach $subDir(@files)
-		{
-		    $subDir_full = File::Spec->catdir($dirJob, $dir, $subDir );
-			if (($subDir ne ".") && ($subDir ne "..") && -d $subDir_full) # si le fichier est de type repertoire
-			{
-				push(@$names,$dir); #récupération nom séquence
-				@position = split(/__/,$subDir);
-				push(@$positions,$position[1]); # récupération position
-				push(@$namesPositions,$subDir); # récupération noms + positions
-				$MfeiExist = false;
-				$RandfoldExist = false;
-				$alignExist = false;
-				$SCExist = false;
-                my $pvalue = File::Spec->catfile( $subDir_full, 'pvalue.txt' );
-                if( -e $pvalue ) # si fichier existe
-                {
-                    $RandfoldExist = true;
-                    open (PVALUE, $pvalue) || die "$!";
-					while (my $line = <PVALUE>) 
-					{
-						if ( $line =~/(.*)\t(.*)\t(.*)/ )
-						{
-							push(@$pvalues,$3); # récupération pvalues
-						}
-					}
-					close PVALUE;
-				}
-				#Récupération valeur MFEI
-				my $mfei_out = File::Spec->catfile( $subDir_full, 'outMFEI.txt' );
-				if( -e $mfei_out ) # si fichier existe
-				{
-					$MfeiExist = true;
-					open (MFEI, $mfei_out) || die "$!";
-					while (my $line = <MFEI>) 
-					{
-						if ( $line =~/(.*)\t(.*)\t(.*)\t(.*)/ )
-						{    
-							push(@$mfeis,$2); # récupération mfei
-							push(@$mfes,$3); # récupération mfei
-							push(@$amfes,$4); # récupération mfei
-						}
-					}
-					close MFEI;
-				}
-				#Récupération valeur self contain
-				my $selfcontain_out = File::Spec->catfile( $subDir_full, 'selfContain.txt' );
-				if( -e $selfcontain_out ) # si fichier existe
-				{
-					$SCExist = true;
-					open (SC, $selfcontain_out) || die "$!";
-					while (my $line = <SC>) 
-					{
-						if ( $line =~/(.*) (.*)/ )
-						{    
-							push(@$selfContain,$2); # récupération mfei
-						}
-					}
-					close SC;
-				}
-					
-				#Récupération séquence et format Vienna
-				my $vienna_out = File::Spec->catfile( $subDir_full, 'outViennaTraited.txt' );
-				if( -e $vienna_out ) # si fichier existe
-                {
-    				open (my $VIENNA, '<', $vienna_out) || die "$!";
-    				while (my $line = <$VIENNA>) 
-    				{
-    					if ( $line =~/(.*)\t(.*)\t(.*)/ )
-    					{    
-    						push(@$DNASequence,$2); # récupération sequence
-    						push(@$Vienna,$3); # récupération Vienna
-    					
-    					}
-    				}
-    				close $VIENNA;
-                }
-				#Récupération alignement avec mirBase 
-				my $alignement = File::Spec->catfile( $subDir_full, 'alignement.txt' );
-				if( -e $alignement ) # si fichier existe
-				{
-					$alignExist = true;
-					open (align, $alignement) || die "$!";
-					$align = "none";
-					while (my $line = <align>) 
-					{
-						if ( $line =~/^C4/ )
-						{    
-							$align = $alignement;
-							last;
-						}
-					
-					}
-					push(@$alignement,$align); 
-				}
-			}
-		}
-	}
-}
-	
-	
-$myResults=results->new(); # initialisation du struct 
-$myResults->names($names);
-$myResults->pvalues($pvalues);
-$myResults->namesPositions($namesPositions);
-$myResults->mfeis($mfeis);
-$myResults->mfes($mfes);
-$myResults->amfes($amfes);
-$myResults->positions($positions);
-$myResults->DNASequence($DNASequence);
-$myResults->Vienna($Vienna);
-$myResults->alignement($alignement);
-$myResults->selfContain($selfContain);
-
-my $csv = PipelineMiRNA::WebFunctions->resultstruct2csv(scalar(@$names), $myResults);
-my $csv_file = File::Spec->catfile($dirJob, 'result.csv');
-open (my $CSV, '>', $csv_file) or die "Error when opening $csv_file: $!";
-print $CSV $csv;
-close $CSV or die "Cannot close $csv_file: $!";
-print <<DATA;
+my $HTML_header = <<'END_TXT';
 Content-type: application/xhtml+xml
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-	<head>
-		<link rel="stylesheet" type="text/css" href="/arn/css/script.css" />
-		<script type="text/javascript" language="Javascript" src="/arn/js/results.js"> </script>
-		<script type="text/javascript" src="/arn/js/graphics.js"></script>
-	<script type="text/javascript" src="/arn/js/miARN.js"></script>	
-	</head>
-	<body onload='main();'>
-		<div class="titreDiv"> Identification of miRNA/miRNA hairpins results:</div>
-DATA
+    <head>
+        <link rel="stylesheet" type="text/css" href="/arn/css/script.css" />
+        <script type="text/javascript" language="Javascript" src="/arn/js/results.js"> </script>
+        <script type="text/javascript" src="/arn/js/graphics.js"></script>
+        <script type="text/javascript" src="/arn/js/miARN.js"></script>
+    </head>
+END_TXT
 
+my $HTML_additional = "";
 if ($name_job ne "")
 {
-	print "<div class='titleJob' ><li>Title Job : ".$name_job."</li></div>";
-	
+    $HTML_additional .= "<div class='titleJob' ><li>Title Job : ".$name_job."</li></div>";
 }
-print <<DATA;
-		<div id="table" ></div>
-		<div id="singleCell"> </div>
-		<results id="all">
-DATA
 
-for ($i=0;$i<scalar(@$names); $i++)
-{
-	$baliseSeq="<Sequence name='";
-	$baliseSeq=$baliseSeq.$myResults->names($i);
-	$baliseSeq=$baliseSeq."' position='".$myResults->positions($i);
-	if ($MfeiExist eq "true")
-	{
-		$baliseSeq=$baliseSeq."' mfei='".$myResults->mfeis($i)."' mfe='".$myResults->mfes($i)."' amfe='".$myResults->amfes($i);
-	}
-	if ($RandfoldExist eq "true")
-	{
-		$baliseSeq=$baliseSeq."'  p_value='".$myResults->pvalues($i);
-	}
-	if ($SCExist eq "true")
-	{
-		$baliseSeq=$baliseSeq."'  self_contain='".$myResults->selfContain($i);
-	}
-	if ($alignExist eq "true")
-	{
-		$baliseSeq=$baliseSeq."'  alignment='".$myResults->alignement($i);
-	}
-	
-		
-	$baliseSeq=$baliseSeq."' image='"."/arn/data/job".$id_job.'/'.$myResults->names($i)."/".$myResults->names($i)."__".$myResults->positions($i)."/image.png' Vienna='".$myResults->Vienna($i)."' DNASequence='".$myResults->DNASequence($i)."'></Sequence>\n";
-	print $baliseSeq;
+my $valid = PipelineMiRNA::WebFunctions->is_valid_jobID($id_job);
 
-	#print "<Sequence name='".$myResults->names($i)."' position='".$myResults->positions($i)."' mfei='".$myResults->mfeis($i)."'  p_value='".$myResults->pvalues($i)."' image='".$myResults->names($i)."_ss.ps' Vienna='".$myResults->Vienna($i)."' DNASequence='".$myResults->DNASequence($i)."'></Sequence>\n";	 
-	#print "<Sequence name='".$myResults->names($i)."' position='".$myResults->positions($i)."' mfei='".$myResults->mfeis($i)."'  p_value='".$myResults->pvalues($i)."' image='".$dirImages.$myResults->names($i)."_ss.ps' ' DNA='".$myResults->DNASequence($i)."' Vienna='".$myResults->Vienna($i)."></Sequence>\n";	
-}
-print <<"DATA";		
-		</results>
-	<a href="./resultsAsCSV.pl?run_id=$id_job">Download as CSV</a>
-	</body>
+if($valid){
+    my %myResults = PipelineMiRNA::WebFunctions->get_structure_for_jobID($id_job);
+    my $HTML_results = PipelineMiRNA::WebFunctions->resultstruct2pseudoXML(\%myResults);
 
+    print <<"HTML";
+$HTML_header    <body onload="main();">
+        <div class="titreDiv"> Identification of miRNA/miRNA hairpins results:</div>
+$HTML_additional
+        <div id="table" ></div>
+        <div id="singleCell"> </div>
+$HTML_results
+    <a href="./resultsAsCSV.pl?run_id=$id_job">Download as CSV</a>
+    </body>
 </html>
+HTML
 
-DATA
-
+}else{
+    print <<"HTML";
+$HTML_header
+    <body>
+        <p>No results available for the given job identifier $id_job: $valid </p>
+    </body>
+</html>
+HTML
+}
--- lib/PipelineMiRNA/WebFunctions.pm
+++ lib/PipelineMiRNA/WebFunctions.pm
@@ -3,37 +3,252 @@ package PipelineMiRNA::WebFunctions;
 use strict;
 use warnings;
 use Data::Dumper;
+use File::Spec;
+use PipelineMiRNA::Paths;
 
-sub resultstruct2csv{
-    my ($self, @args) = @_;
-    my $size = shift @args;
-    my $results = shift @args;
-    
-    my @headers= ('Name',
-                  'Positions',
-                  'MFEI',
-                  'MFE',
-                  'AMFE',
-                  'Pvalue',
-                  'sequence',
-                  'secondary structure',
-                  );
-    my $res =  join( ',', @headers ) . "\n";
-    for (my $i=0; $i<$size; $i++)
+my @headers = ('name', 'position', 'mfei', 'mfe', 'amfe', 'p_value', 'self_contain', 'alignment', 'image', 'Vienna', 'DNASequence');
+
+=method jobId_to_jobPath
+
+Get the job path from a job identifier
+
+=cut
+
+sub jobId_to_jobPath {
+    my ( $self, @args ) = @_;
+    my $id_job      = shift @args;
+    my $dirJob_name = 'job' . $id_job;
+    my $jobPath = File::Spec->catdir( 'results', $dirJob_name);
+    return $jobPath;
+}
+
+=method is_valid_jobID
+
+Test whether a jobID is valid - ie if there are results for it.
+
+=cut
+
+sub is_valid_jobID {
+    my ( $self, @args ) = @_;
+    my $id_job          = shift @args;
+    my $jobPath = $self->jobId_to_jobPath($id_job);
+    my $full_path = PipelineMiRNA::Paths->get_absolute_path($jobPath);
+    return (-e $full_path);
+}
+
+=method get_structure_for_jobID
+
+Get the results structure of a given job identifier
+
+=cut
+
+sub get_structure_for_jobID {
+    my ( $self, @args ) = @_;
+    my $jobId   = shift @args;
+    my $job = $self->jobId_to_jobPath($jobId);
+    my $job_dir = PipelineMiRNA::Paths->get_absolute_path($job);
+    my %myResults = ();
+
+    opendir DIR, $job_dir;    #ouverture répertoire job
+    my @dirs;
+    @dirs = readdir DIR;
+    closedir DIR;
+    foreach my $dir (@dirs)    # parcours du contenu
     {
-        my @array = ($results->names($i),
-                     $results->positions($i),
-                     $results->mfeis($i),
-                     $results->mfes($i),
-                     $results->amfes($i),
-                     $results->pvalues($i),
-                     $results->DNASequence($i),
-                     $results->Vienna($i),
-                     $results->selfContain($i),
-                     );
-        $res .= join( ',', @array ) . "\n";
+        my $full_dir = File::Spec->catdir( $job_dir, $dir );
+        if (    $dir ne "."
+             && $dir ne ".."
+             && -d $full_dir )    #si fichier est un répertoire
+        {
+            opendir DIR, $full_dir;    # ouverture du sous répertoire
+            my @files;
+            @files = readdir DIR;
+            closedir DIR;
+            foreach my $subDir (@files) {
+                my $subDir_full = File::Spec->catdir( $job_dir, $dir, $subDir );
+                if (    ( $subDir ne "." )
+                     && ( $subDir ne ".." )
+                     && -d $subDir_full ) # si le fichier est de type repertoire
+                {
+                    $myResults{$subDir} = ();
+
+                    $myResults{$subDir}{'name'} = $dir;    #récupération nom séquence
+                    my @position = split( /__/, $subDir );
+                    $myResults{$subDir}{'position'} = $position[1]; # récupération position
+                      ;    # récupération noms + positions
+                    my $pvalue =
+                      File::Spec->catfile( $subDir_full, 'pvalue.txt' );
+
+                    if ( -e $pvalue )    # si fichier existe
+                    {
+                        open( PVALUE, $pvalue ) || die "$!";
+                        while ( my $line = <PVALUE> ) {
+                            if ( $line =~ /(.*)\t(.*)\t(.*)/ ) {
+                                $myResults{$subDir}{'p_value'} = $3;  # récupération pvalues
+                            }
+                        }
+                        close PVALUE;
+                    }
+
+                    #Récupération valeur MFEI
+                    my $mfei_out =
+                      File::Spec->catfile( $subDir_full, 'outMFEI.txt' );
+                    if ( -e $mfei_out )                 # si fichier existe
+                    {
+                        open( MFEI, $mfei_out ) || die "$!";
+                        while ( my $line = <MFEI> ) {
+                            if ( $line =~ /(.*)\t(.*)\t(.*)\t(.*)/ ) {
+                                $myResults{$subDir}{'mfei'} = $2;    # récupération mfei
+                                $myResults{$subDir}{'mfe'} = $3;    # récupération mfei
+                                $myResults{$subDir}{'amfe'} = $4;    # récupération mfei
+                            }
+                        }
+                        close MFEI;
+                    }
+
+                    #Récupération valeur self contain
+                    my $selfcontain_out =
+                      File::Spec->catfile( $subDir_full, 'selfContain.txt' );
+                    if ( -e $selfcontain_out )          # si fichier existe
+                    {
+                        open( SC, $selfcontain_out ) || die "$!";
+                        while ( my $line = <SC> ) {
+                            if ( $line =~ /(.*) (.*)/ ) {
+                                $myResults{$subDir}{'self_contain'} = $2; # récupération mfei
+                            }
+                        }
+                        close SC;
+                    }
+
+                    #Récupération séquence et format Vienna
+                    my $vienna_out = File::Spec->catfile( $subDir_full,
+                                                       'outViennaTraited.txt' );
+                    if ( -e $vienna_out )                  # si fichier existe
+                    {
+                        open( my $VIENNA, '<', $vienna_out ) || die "$!";
+                        while ( my $line = <$VIENNA> ) {
+                            if ( $line =~ /(.*)\t(.*)\t(.*)/ ) {
+                                $myResults{$subDir}{'DNASequence'} = $2;
+                                  ;    # récupération sequence
+                                $myResults{$subDir}{'Vienna'} = $3;    # récupération Vienna
+
+                            }
+                        }
+                        close $VIENNA;
+                    }
+
+                    #Récupération alignement avec mirBase
+                    my $file_alignement =
+                      File::Spec->catfile( $subDir_full, 'alignement.txt' );
+                    if ( -e $file_alignement )                # si fichier existe
+                    {
+                        open( my $ALIGN, $file_alignement ) || die "$!";
+                        my $align = "none";
+                        while ( my $line = <$ALIGN> ) {
+                            if ( $line =~ /^C4/ ) {
+                                $align = $file_alignement;
+                                last;
+                            }
+                        }
+                        $myResults{$subDir}{'alignment'} = $align;
+                        close $ALIGN or die ("Error closing $file_alignement: $!");
+                    }
+                    my $image_path = PipelineMiRNA::Paths->get_server_path($job,  $dir, $subDir, 'image.png');
+                    $myResults{$subDir}{'image'} = $image_path;
+                }
+            }
+        }
     }
-    return $res;
+
+    return %myResults;
+}
+
+=method resultstruct2csv
+
+Convert the results structure to CSV
+
+=cut
+
+sub resultstruct2csv {
+    my ( $self, @args ) = @_;
+    my $results = shift @args;
+    my %results = %{$results};
+    my @headers = ('name', 'position', 'mfei', 'mfe', 'amfe', 'p_value', 'self_contain', 'Vienna', 'DNASequence');
+
+    my $result = join( ',', @headers ) . "\n";
+    while ( my ($key, $value) = each %results )
+    {
+        for my $header (@headers){
+            $result .= "${$value}{$header},";
+        }
+        $result .= "\n";
+    }
+    return $result;
+}
+
+=method resultstruct2table
+
+Convert the results structure to HTML table
+
+=cut
+
+sub resultstruct2table {
+    my ( $self, @args ) = @_;
+    my $results = shift @args;
+    my %results = %{$results};
+
+    my $HTML_results = <<'END_TXT';
+            <div class="titreDiv"> Identification of miRNA/miRNA hairpins results:</div>
+            <div id="table" ></div>
+END_TXT
+
+    my $row = 0;
+    my $column = -1;
+    $HTML_results .= "<table>\n<tbody>";
+    $HTML_results .= "<tr>";
+    for my $header (@headers){
+        $column += 1;
+        my $th_content = "id='cell-$row-$column' width='100' onclick='showCellInfo($row, $column)' onmouseover='colorOver($row, $column)' onmouseout='colorOut($row, $column)'";
+        $HTML_results .= "<th $th_content>$header</th>\n";
+    }
+    $HTML_results .= "</tr>\n";
+    while ( my ($key, $value) = each %results )
+    {
+        $row += 1;
+        $column = -1;
+      $HTML_results .= '<tr>';
+      for my $header (@headers){
+          $column += 1;
+          my $td_content = "id='cell-$row-$column' onmouseover='colorOver($row, $column)' onmouseout='colorOut($row, $column)' onclick='showCellInfo($row, $column)'";
+          $HTML_results .= "<th $td_content>${$value}{$header}</th>\n";
+      }
+      $HTML_results .= "\n</tr>\n";
+    }
+    $HTML_results .= "</tbody></table>";
+    return $HTML_results;
+}
+
+=method resultstruct2pseudoXML
+
+Convert the results structure to to pseudo XML format
+
+=cut
+
+sub resultstruct2pseudoXML {
+    my ( $self, @args ) = @_;
+    my $results = shift @args;
+    my %results = %{$results};
+    my $result = "<results id='all'>\n";
+    while ( my ($key, $value) = each %results )
+    {
+        $result .= "<Sequence";
+        for my $header (@headers){
+            $result .= " $header='${$value}{$header}'";
+        }
+        $result .= "></Sequence>\n";
+    }
+    $result .= "</results>";
+    return $result;
 }
 
 1;
