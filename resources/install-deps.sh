938b90bd32e94ab5bb76dd26b160ad8e3aa459da
JeanFred <jeanfred1@gmail.com>
2013-09-11 09:14:35 +0000

    PipelineMiRNA: Account for relative path in shell scripts
    In both install-deps.sh & run-tests.sh, we want to execute
    fellow shell script use-locallib.sh ; but we might not
    (and will not) execute it from the actual directory.
    
    Retrieve the basedir and concatenate it to the script name.

--- resources/install-deps.sh
+++ resources/install-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+BASEDIR=$(dirname $0)
 perl --version
 cpanm --version
-cpanm --local-lib=~/perl5 local::lib && sh use-locallib.sh
+cpanm --local-lib=~/perl5 local::lib && sh $BASEDIR/use-locallib.sh
 cpanm --quiet --notest --skip-satisfied Dist::Zilla
 dzil authordeps | cpanm --quiet --notest --skip-satisfied
 dzil listdeps | cpanm --quiet
--- resources/run-tests.sh
+++ resources/run-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-sh use-locallib.sh
+BASEDIR=$(dirname $0)
+sh $BASEDIR/use-locallib.sh
 prove --r --lib --timer --formatter=TAP::Formatter::JUnit t/ | tee test-results.xml
 sh t/functional/run-functional-tests.sh | tee functional-test-results.txt
