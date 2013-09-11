2240908ddbeb85e5d08d649e30b66eace281e3e5
JeanFred <jeanfred1@gmail.com>
2013-09-11 09:10:37 +0000

    PipelineMiRNA: Use standard local::lib in deps script
    By default, local::lib stuff works in ~/perl5.
    Let’s not change that and make our life more difficult.

--- resources/install-deps.sh
+++ resources/install-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 perl --version
 cpanm --version
-cpanm --local-lib=~/perl local::lib && eval `perl -I ~/perl/lib/perl5/ -Mlocal::lib=~/perl`
+cpanm --local-lib=~/perl5 local::lib && sh use-locallib.sh
 cpanm --quiet --notest --skip-satisfied Dist::Zilla
 dzil authordeps | cpanm --quiet --notest --skip-satisfied
 dzil listdeps | cpanm --quiet
+
