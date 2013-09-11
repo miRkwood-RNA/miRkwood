254c47947b9266e4536400dafb57618292578bf8
JeanFred <jeanfred1@gmail.com>
2013-09-11 09:08:18 +0000

    PipelineMiRNA: Add Shell script for installation
    We need to install all dependencies as part of the build process,
    and in a manner that does not require root access
    The install-deps script:
    - install local::lib
    - calls the use-locallib script
    - installs DistZilla
    - with DistZilla, lists its own deps and fetches them with cpan minus
    - with DistZilla, lists the package deps and fetches them with cpan minus
    
    The use-locallib script sets local::lib for the current shell
    
    It is also called by run-tests

--- resources/run-tests.sh
+++ resources/run-tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+sh use-locallib.sh
 prove --r --lib --timer --formatter=TAP::Formatter::JUnit t/ | tee test-results.xml
 sh t/functional/run-functional-tests.sh | tee functional-test-results.txt
---
+++ resources/use-locallib.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+eval `perl -I ~/perl5/lib/perl5/ -Mlocal::lib=~/perl5`
