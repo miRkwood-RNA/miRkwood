7d4ccf24639d7429af620f77602e0a41cb4fa789
JeanFred <jeanfred1@gmail.com>
2013-11-20 17:57:43 +0000

    PipelineMiRNA: remove useless files

--- test.php
+++
@@ -1,3 +0,0 @@
-<?php
-exec ("./test.sh");
-?>
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -cp /var/www/arn/scripts/VARNAv3-8.jar fr.orsay.lri.varna.applications.VARNAcmd -i /var/www/arn/data/jobJul61748472012/contig24763/contig24763_470-568/outB2ct.ct -o /var/www/arn/data/jobJul61748472012/contig24763/contig24763_470-568/image.png
-
-
