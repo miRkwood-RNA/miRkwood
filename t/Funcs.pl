ba1024f269a8181dea2e2681ce5dda6779955510
JeanFred <jeanfred1@gmail.com>
2013-11-22 17:44:48 +0000

    PipelineMiRNA: Add method to slurp a file into a scalar
    We need to easily slurp output files for our unit tests.
    Let's make it a method and have in Funcs.pl to be use by all modules.

--- t/Funcs.pl
+++ t/Funcs.pl
@@ -1,5 +1,18 @@
+use strict;
+use warnings;
+
 sub input_file {
-    return File::Spec->catfile($FindBin::Bin, 'data', @_);
+    my @args = @_;
+    return File::Spec->catfile( $FindBin::Bin, 'data', @args );
+}
+
+sub slurp_file {
+    my @args = @_;
+    my $file = shift @args;
+    open my $fh, '<', $file or die $!;
+    my $contents = do { local $/; <$fh> };
+    close $fh or die $!;
+    return $contents;
 }
 
 1;
