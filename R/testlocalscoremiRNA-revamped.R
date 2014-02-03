e001c3b3385e321c19ac633ffd57779100d86933
JeanFred <jeanfred1@gmail.com>
2014-02-03 21:02:56 +0000

    PipelineMiRNA: Add refactored of Guillemette script
    This version is more structured, and can be used as a script.

---
+++ R/testlocalscoremiRNA-revamped.R
@@ -0,0 +1,53 @@
+#!/usr/bin/Rscript
+#Usage:
+#Rscript testlocalscoremiRNA-test.R <working_dir> <bedfile>
+
+# Loading library
+library(pvalues)
+
+# Reading arguments
+args<-commandArgs(TRUE)
+working_directory = args[1]
+bedfile = args[2]
+
+# Setting global variables
+seuil = 70
+posdep = 0
+nbpermutations = 5
+
+#
+calcfinalres=function(x, start_position, minadjtags=2, permutations_number=nbpermutations){
+  score=localScore(x)
+  if(nrow(score)==0){resScLoc=NULL}else{
+    lengthsig=length(x)-c(0,cumsum(score[,"length"])[-nrow(score)])
+    subseqMatrix=cbind("nbmeas"=score[,"length"], "lengthsig"=lengthsig, "localscore"=score[,"score"])
+    keeppos=which(subseqMatrix[,"nbmeas"]>=minadjtags)
+    resScLoc=data.frame(score[keeppos,],pvalueResampling(subseqMatrix[keeppos,], permutations_number, x, nbThreads=1, seed=123))
+  }
+  signifreg=resScLoc[which(resScLoc$pvalue<=0.05),]
+  finalres=data.frame("startreg"=signifreg$start+start_position,"endreg"=signifreg$end+start_position,signifreg)
+  finalres
+}
+
+processChr = function(counts, start_position){
+    xint=counts-seuil
+    finalres = calcfinalres(xint, start_position=start_position)
+    finalres[,c("startreg","endreg")]
+}
+
+
+main=function(file_path, threshold, start_position){
+    signal = read.table(file_path)
+    setwd(working_directory)
+    cat("Start",file="0-start.txt",sep="\n")
+
+    counts_chr = signal[,5]
+    res_chr = processChr(counts_chr, start_position)
+    cat("Chr",file="1-chr1.txt",sep="\n")
+    resTogive = data.frame("chr"="chr1", res_chr1)
+    write.table(resTogive, "peaksfound.txt", row.names=FALSE)
+    resTogive
+}
+
+result = main(bedfile, seuil, posdep)
+
