f2b116dca411232ef243a654e8ae78251885d5c8
JeanFred <jeanfred1@gmail.com>
2014-02-07 20:51:22 +0000

    PipelineMiRNA: Forgot some Chrs in Shell script piloting R

--- R/run.sh
+++ R/run.sh
@@ -3,11 +3,11 @@ ROOT_FOLDER=results
 WORK_FOLDER=~/Tuile/pipelineMiRNA-web/R
 for THRESHOLD in 5 10 20 100 200 500
 do
-	for CHR in ChrM ChrC
+	for CHR in ChrM ChrC Chr1 Chr2 Chr3 Chr4 Chr5
 	do
 #		touch data/$CHR.bed
 #		bedtools coverage -d -abam tutorial.bam -b $CHR.bed > $CHR.bedgraph
-		FOLDER=$ROOT_FOLDER/Threshold$THRESHOLD
+		FOLDER=$ROOT_FOLDER/Threshold-$THRESHOLD
 		mkdir -p $FOLDER
 		Rscript $WORK_FOLDER/testlocalscoremiRNA-revamped.R $FOLDER $WORK_FOLDER/data/$CHR.bedgraph $CHR $THRESHOLD
 	done
