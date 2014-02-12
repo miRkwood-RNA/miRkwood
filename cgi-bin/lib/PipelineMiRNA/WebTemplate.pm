48dda9fcf9581f6b2931462fa21b82bf4b8f2133
JeanFred <jeanfred1@gmail.com>
2014-02-12 20:39:38 +0000

    PipelineMiRNA: Factorise HTML in WebTemplate
    Add two methods to be the poor man templating system.
    - One takes the <body> content and returns the all HTML
    - one takes the central <div>, put the menues around to make a body,
      and call the above.
    
    We had to be smart for the CSS/JS files: methods take an array
    of JS files and another of CSS files, and make up as many <link> and
    <script> elements as needed.

--- cgi-bin/lib/PipelineMiRNA/WebTemplate.pm
+++ cgi-bin/lib/PipelineMiRNA/WebTemplate.pm
@@ -177,4 +177,68 @@ END_TXT
     return $HTML
 }
 
+sub get_HTML_page_for_content {
+    my @args      = @_;
+    my $page      = shift @args;
+    my $css_files = shift @args;
+    my $js_files  = shift @args;
+    my $no_menu   = shift @args;
+
+    my $bioinfo_menu = '';
+    if (! $no_menu){
+        $bioinfo_menu = PipelineMiRNA::WebTemplate::get_bioinfo_menu();
+    }
+
+    my $header_menu  = PipelineMiRNA::WebTemplate::get_header_menu();
+    my $footer       = PipelineMiRNA::WebTemplate::get_footer();
+
+    my $body = <<"END_TXT";
+    <body>
+        <div class="theme-border"></div>
+        <div class="logo"></div>
+        $bioinfo_menu
+        <div class="bloc_droit">
+        $header_menu
+            $page
+        $footer
+        </div><!-- bloc droit-->
+    </body>
+END_TXT
+    my $HTML = get_HTML_page_for_body($body, $css_files, $js_files);
+    return $HTML;
+}
+
+sub get_HTML_page_for_body {
+    my @args      = @_;
+    my $body      = shift @args;
+    my @css_files = @{shift @args};
+    my @js_files  = @{shift @args};
+
+    my $css_html = '';
+    foreach my $css (@css_files){
+        $css_html .= "<link type='text/css' rel='stylesheet' href='$css' />";
+    }
+
+    my $js_html = '';
+    foreach my $js (@js_files){
+        $js_html .= "<script type='text/javascript' src='$js'></script>";
+    }
+
+    my $HTML = <<"END_TXT";
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+        <meta name="keywords" content="RNA, ARN, mfold, fold, structure, prediction, secondary structure" />
+        <title>miREST - MicroRNA identification</title>
+        $css_html
+        $js_html
+    </head>
+    $body
+</html>
+END_TXT
+
+    return $HTML;
+}
+
 1;
