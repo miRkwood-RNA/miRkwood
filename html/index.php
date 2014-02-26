fa64a05759ca9668b19c77974cbbf91dea273588
Mohcen Benmounah <mohcen.benmounah@inria.fr>
2014-02-26 15:39:40 +0000

    deplacement dossier static dans /html + ajout de la page index.php

---
+++ html/index.php
@@ -0,0 +1,20 @@
+  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+        <link type='text/css' rel='stylesheet' href='./Style/bioinfo.css' />
+		<link type='text/css' rel='stylesheet' href='./style/script.css' />
+		<link type='text/css' rel='stylesheet' href='./style/rna.css' />
+        <script type='text/javascript' src='/js/miARN.js'></script>
+        <title>MicroRNA identification</title>
+    </head>
+ <body>
+        <div class="theme-border"></div>
+        <div class="logo"></div>
+         <? include("./static/bioinfo_menu.txt") ?>
+        <div class="bloc_droit">
+        <? include("./static/header_menu.txt") ?>
+            
+        </div><!-- bloc droit-->
+       <? include("./static/footer.txt") ?>
+    </body>
+    
+    </html>
