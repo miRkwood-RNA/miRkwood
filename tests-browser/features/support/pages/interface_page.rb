b988e6708bbeb379eceb45c05a894dd39d1e43da
JeanFred <jeanfred1@gmail.com>
2014-03-06 14:26:01 +0000

    miRkwood: Add clear feature to InterfacePage object

--- tests-browser/features/support/pages/interface_page.rb
+++ tests-browser/features/support/pages/interface_page.rb
@@ -5,6 +5,7 @@ class InterfacePage
   page_url URL.url() + 'interface.pl'
 
   a('example_button', :id => "seq_button")
+  a('area_clear', :id => "area_clear")
   button('run_button', :id => "upload")
   textarea('sequence_area', :id => "seqArea")
 
