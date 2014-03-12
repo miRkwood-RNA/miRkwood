275977c411cd48d2f53e9866ff08e252a77c7be9
JeanFred <jeanfred1@gmail.com>
2014-03-12 14:55:08 +0000

    miRkwood: Update default BioInfo URL
    We changed the path to miRkwood on BioInfoTest.
    Update default URL accordingly

--- tests-browser/features/support/modules/url_module.rb
+++ tests-browser/features/support/modules/url_module.rb
@@ -3,7 +3,7 @@ module URL
     if ENV["MIRKWOOD_URL"]
       bioinfo_url = ENV["MIRKWOOD_URL"]
     else
-      bioinfo_url = 'http://bioinfotest.lifl.fr/cgi-bin/MiRNA/web_scripts/'
+      bioinfo_url = 'http://bioinfotest.lifl.fr/cgi-bin/mirkwood/web_scripts/'
     end
     bioinfo_url
   end
