5a26ad7efe4d3902d68a812859ae17a13c45f88c
JeanFred <jeanfred1@gmail.com>
2014-04-02 12:31:48 +0000

    miRkwood: FilterCDS script must initialize programs first
    Otherwise the blastx_bin variable is empty.

--- cgi-bin/scripts/filterCDS.pl
+++ cgi-bin/scripts/filterCDS.pl
@@ -1,5 +1,7 @@
 use PipelineMiRNA::Components;
+use PipelineMiRNA::Programs;
 
 my ( $idirData, $idirJob, $iplant ) = @ARGV;
 
+PipelineMiRNA::Programs::init_programs();
 PipelineMiRNA::Components::filter_CDS( $idirData, $idirJob, $iplant );
