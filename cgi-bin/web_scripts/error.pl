c8a00b7f924cd3e6cbe475953abe1d75a98025c0
JeanFred <jeanfred1@gmail.com>
2014-04-09 14:44:20 +0000

    miRkwood: Update error.pl page

--- cgi-bin/web_scripts/error.pl
+++ cgi-bin/web_scripts/error.pl
@@ -13,32 +13,12 @@ my @js  = (PipelineMiRNA::WebTemplate->get_js_file());
 my $page = <<"END_TXT";
 <div class="main">
   <div id="page">
-    <h2>Error!</h2>
-    <div class="section">
-      miRkwood could not recognize the format of one or more sequences.
-    </div>
-      <p>Your data set should include <b>at least two</b> distinct sequences.</p>
-      <p> Sequences should be in <b>FASTA</b> format.
-        A sequence in FASTA format consists of a
-        single-line description, followed by lines of sequence
-        data. The first character of the description line is a
-        greater-than (&quot;&gt;&quot;) symbol in the first
-        column. All lines should be shorter than 80 characters. An
-        example in FASTA format is:</p>
-<pre>
-> Name of the sequence 1
-ctgcgagcgcgcgatgatagcgcggcgagcatgtagcatgctagctgtcgcgagcact
-cggccgagatcaggcgatgcatgcgcagggagcagcgagcgacgagcacagcatgctagctagatgcatgctgtaggcagc
-cgccgagagacgatggagctgc
-> Name of the sequence 2
-gacagatacgataagaggacgggatagaacgtagacatcgccgagagacgatggagctgc
-cggccgagatcaggcgatgcatgcgcagggagcaggcgagcatgtagcatgctagctgtcgcgagcact
-</pre>
-      <p><b>Lower-case and upper-case</b> letters are both accepted.</p>
-      <p>The full  standard <b>IUPAC</b> nucleic acid code is not supported : only <tt>A</tt>, <tt>C</tt>, <tt>G</tt>, <tt>T</tt> and <tt>U</tt> symbols are recognized.</p>
-      <p><b>Numerical digits <tt>0</tt>, ..., <tt>9</tt>, <tt>-</tt> and dot <tt>.</tt></b> symbols are accepted. They are simply ignored by miRkwood.</p>
+    <h2>Error</h2>
+      <p>An error occured when processing your data: miRkwood could not recognize the format of one or more sequences.</p>
+      <p> Sequences should be in <b>FASTA</b> format. Lower-case and upper-case letters are both accepted.
+      The full  standard I UPAC nucleic acid code is not supported : only <tt>A</tt>, <tt>C</tt>, <tt>G</tt>, <tt>T</tt> and <tt>U</tt> symbols are recognized.</p>
     <br /><br />
-    <div class="home-made-center">
+    <div class="center">
       <input type="button" name="nom" value="Submit a new job" onclick="window.location.href='./interface.pl'" />
     </div>
   </div>
