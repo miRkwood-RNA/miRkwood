1ff0d46c2fc5b848d7f3e7ee46e551ba35f5cf51
JeanFred <jeanfred1@gmail.com>
2014-04-16 18:03:03 +0000

    miRkwood: Resurrect functional tests
    We kind of abandoned our functional tests quite some time ago...
    even before the major architural change of r8782 (December 2013)
    Let us resurrect them.
    
    Rename and move lots of data results around.
    Use mirkwood.pl bin script rather than execute_scripts.pl

--- cgi-bin/t/functional/run-functional-tests.sh
+++ cgi-bin/t/functional/run-functional-tests.sh
@@ -26,17 +26,17 @@ plan 3
 ### Testing script of the PipelineMiRNA
 EXCLUDES="--exclude=.svn --exclude=pvalue.txt --exclude=outBlast.txt --exclude=*miRdupOutput.txt --exclude=*.log --exclude=*.cfg"
 
-rm -rf $BASEDIR/output/filtercds/ && mkdir -p $BASEDIR/output/filtercds/ && cp $BASEDIR/data/filtercds_in.fas $BASEDIR/output/filtercds/sequenceUpload.fas
+rm -rf $BASEDIR/output/filtercds/ && mkdir -p $BASEDIR/output/filtercds/ && cp $BASEDIR/data/filtercds_in.fas $BASEDIR/output/filtercds/input_sequences.fas
 perl -I$ROOTDIR/lib $ROOTDIR/scripts/filterCDS.pl $ROOTDIR/data/ $BASEDIR/output/filtercds/ ATpepTAIR10
 ok 'FilterCDS' [ `diff $EXCLUDES -qr $BASEDIR/expected/filtercds/ $BASEDIR/output/filtercds/ | wc -l` -eq 0 ]
 
-rm -rf $BASEDIR/output/fullpipeline1/ && mkdir -p $BASEDIR/output/fullpipeline1/ && cp $BASEDIR/data/sequenceSomething.fas $BASEDIR/output/fullpipeline1/sequenceUpload.fas
-perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl unChecked strandChecked mfeiChecked randfoldChecked UNalignChecked $BASEDIR/output/fullpipeline1/ ATpepTAIR10
-DIFF=$(diff $EXCLUDES $IGNORES -qr $BASEDIR/output/fullpipeline1/ $BASEDIR/expected/fullpipeline1/ | wc -l)
+rm -rf $BASEDIR/output/fullpipeline1/
+perl -I$ROOTDIR/lib $ROOTDIR/bin/mirkwood.pl --output $BASEDIR/output/fullpipeline1/ $BASEDIR/data/sequenceSomething.fas --align --no-process
+DIFF=$(diff $EXCLUDES -qr $BASEDIR/output/fullpipeline1/ $BASEDIR/expected/fullpipeline1/ | wc -l)
 ok 'Full pipeline' [ $DIFF -eq 0 ]
 
-rm -rf $BASEDIR/output/fullpipeline2/ && mkdir -p $BASEDIR/output/fullpipeline2/ && cp $BASEDIR/data/filtercds_in.fas $BASEDIR/output/fullpipeline2/sequenceUpload.fas
-perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl checked  strandChecked mfeiChecked randfoldChecked UNalignChecked $BASEDIR/output/fullpipeline2/ ATpepTAIR10
+rm -rf $BASEDIR/output/fullpipeline2/
+perl -I$ROOTDIR/lib $ROOTDIR/bin/mirkwood.pl --output $BASEDIR/output/fullpipeline2/ $BASEDIR/data/filtercds_in.fas --align --no-process --species-mask ATpepTAIR10
 DIFF=$(diff $EXCLUDES -I 'fullpipeline' -qr $BASEDIR/output/fullpipeline2/ $BASEDIR/expected/fullpipeline2/ | wc -l)
 ok 'Full pipeline with FilteringCDS' [ $DIFF -eq 0 ]
 
