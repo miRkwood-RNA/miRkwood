992904fd5b0b843c7a8e76469220155204cf3c6e
JeanFred <jeanfred1@gmail.com>
2014-04-18 20:50:04 +0000

    miRkwood: Update Shell installation script
    Exonerate does not need to be installed as we distribute it.
    Blast and the Vienna package are assumed to be installed through
    byt the user through the package manager

--- cgi-bin/install-programs.sh
+++ cgi-bin/install-programs.sh
@@ -11,26 +11,16 @@ fi
 ROOT_PATH=$(readlink -f $1)
 cd $ROOT_PATH
 
-echo "Installing ViennaRNA 2.1.2"
-wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.1.2.tar.gz
-tar xf ViennaRNA-2.1.2.tar.gz
-cd ViennaRNA-2.1.2
-./configure
-make
-cd $ROOT_PATH
+#echo "Installing ViennaRNA 2.1.2"
+#wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.1.2.tar.gz
+#tar xf ViennaRNA-2.1.2.tar.gz
+#cd ViennaRNA-2.1.2
+#./configure
+#make
+#cd $ROOT_PATH
 
 echo "Installing VARNA 3.9"
 wget http://varna.lri.fr/bin/VARNAv3-9.jar
-cd $ROOT_PATH
-
-echo "Installing Exonerate"
-#wget https://www.ebi.ac.uk/%7Eguy/exonerate/exonerate-2.2.0.tar.gz
-#tar xf exonerate-2.2.0.tar.gz
-#cd exonerate-2.2.0
-#./configure
-#make
-wget https://www.ebi.ac.uk/%7Eguy/exonerate/exonerate-2.2.0-i386.tar.gz
-tar xf exonerate-2.2.0-i386.tar.gz
 cd $ROOT_PATH
 
 echo "Installing MiRdup 1.2"
@@ -38,14 +28,6 @@ mkdir miRdup_1.2
 cd miRdup_1.2
 wget http://www.cs.mcgill.ca/~blanchem/mirdup/miRdup_1.2.zip
 unzip miRdup_1.2.zip
-cd $ROOT_PATH
-
-echo "Installing NCBI Blast 2.2.28"
-wget ftp://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/LATEST/ncbi-blast-2.2.28+-src.tar.gz
-tar xf ncbi-blast-2.2.28+-src.tar.gz
-cd ncbi-blast-2.2.28+-src/c++/
-./configure
-make
 cd $ROOT_PATH
 
 rm *.gz *.zip
