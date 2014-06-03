9634b3bf7164da1430206b4d41e5aaee6cdc5c6a
JeanFred <jeanfred1@gmail.com>
2014-06-03 13:00:03 +0000

    miRkwood: Avoid installing already installed programs
    Two of the programs, miRdup and VARNA, are expected in the programs
    directory and this script installs them.
    The script should not try to reinstall these programs if they
    are already there.

--- cgi-bin/install-programs.sh
+++ cgi-bin/install-programs.sh
@@ -11,23 +11,26 @@ fi
 ROOT_PATH=$(readlink -f $1)
 cd $ROOT_PATH
 
-#echo "Installing ViennaRNA 2.1.2"
-#wget http://www.tbi.univie.ac.at/~ronny/RNA/ViennaRNA-2.1.2.tar.gz
-#tar xf ViennaRNA-2.1.2.tar.gz
-#cd ViennaRNA-2.1.2
-#./configure
-#make
-#cd $ROOT_PATH
+if [ ! -f $ROOT_PATH/VARNAv3-9.jar ]
+then
+    echo "Installing VARNA 3.9"
+    wget http://varna.lri.fr/bin/VARNAv3-9.jar
+    cd $ROOT_PATH
+else
+    echo "VARNA 3.9 already installed"
+fi
 
-echo "Installing VARNA 3.9"
-wget http://varna.lri.fr/bin/VARNAv3-9.jar
-cd $ROOT_PATH
+MIRDUP=$ROOT_PATH/miRdup_1.2
+if [ ! -e $MIRDUP ]
+then
+    echo "Installing MiRdup 1.2"
+    mkdir $MIRDUP
+    cd $MIRDUP
+    wget http://www.cs.mcgill.ca/~blanchem/mirdup/miRdup_1.2.zip
+    unzip miRdup_1.2.zip
+    cd $ROOT_PATH
+else
+    echo "MiRdup 1.2 already installed"
+fi
 
-echo "Installing MiRdup 1.2"
-mkdir miRdup_1.2
-cd miRdup_1.2
-wget http://www.cs.mcgill.ca/~blanchem/mirdup/miRdup_1.2.zip
-unzip miRdup_1.2.zip
-cd $ROOT_PATH
-
-rm *.gz *.zip
+rm -f *.gz *.zip
