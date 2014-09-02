345ad04e41a6b50b65ac21e9e748cb9e81ebdd68
JeanFred <jeanfred1@gmail.com>
2014-09-02 10:04:59 +0000

    Fix quick run script

--- cgi-bin/t/functional/quick-run.sh
+++ cgi-bin/t/functional/quick-run.sh
@@ -5,5 +5,5 @@ ROOTDIR=$BASEDIR/../..
 
 TESTFOLDER=$BASEDIR/output/quicktest
 
-rm -rf $TESTFOLDER && mkdir -p $TESTFOLDER && cp $BASEDIR/data/sequenceSomething.fas $TESTFOLDER/sequenceUpload.fas
-perl -I$ROOTDIR/lib $ROOTDIR/scripts/execute_scripts.pl 0 0 1 1 1 $TESTFOLDER ATpepTAIR10
+rm -rf $TESTFOLDER && mkdir -p $TESTFOLDER
+perl -I$ROOTDIR/lib $ROOTDIR/bin/mirkwood.pl --output $TESTFOLDER $BASEDIR/data/sequenceSomething.fas --no-process
