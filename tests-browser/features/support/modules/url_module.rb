cfbf1e31fadf551dff7fa2ec47b1503521fcfe0f
JeanFred <jeanfred1@gmail.com>
2014-09-23 19:38:37 +0000

    miRkwood: Set default Watir URL to Bioinfo
    Bioinfotest is not accessible most of the time,
    which might give the wrong impression :)

--- tests-browser/features/support/modules/url_module.rb
+++ tests-browser/features/support/modules/url_module.rb
@@ -1,9 +1,9 @@
 module URL
   def self.url()
     if ENV["MIRKWOOD_URL"]
-      bioinfo_url = ENV["MIRKWOOD_URL"]
+      mirkwood_url = ENV["MIRKWOOD_URL"]
     else
-      bioinfo_url = 'http://bioinfotest.lifl.fr/cgi-bin/mirkwood/web_scripts/'
+      mirkwood_url = 'http://bioinfo.lifl.fr/cgi-bin/mirkwood/web_scripts/'
     end
     bioinfo_url
   end
@@ -12,7 +12,7 @@ module URL
     if ENV["MIRKWOOD_HOME_URL"]
       mirkwood_home_url = ENV["MIRKWOOD_HOME_URL"]
     else
-      mirkwood_home_url = 'http://bioinfotest.lifl.fr/mirkwood/'
+      mirkwood_home_url = 'http://bioinfo.lifl.fr/mirkwood/'
     end
     mirkwood_home_url
   end
