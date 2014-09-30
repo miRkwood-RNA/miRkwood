74a00a9811aecf8dec5e07523b904e89667d618d
JeanFred <jeanfred1@gmail.com>
2014-09-30 19:14:04 +0000

    miRkwood: Expand PageObject for results page

--- tests-browser/features/support/pages/results_page.rb
+++ tests-browser/features/support/pages/results_page.rb
@@ -1,8 +1,21 @@
 class ResultsPage
   include PageObject
 
-  div('results', :id => "table")
+  @params = { :run_id => '' }
+  include URL
+  page_url "#{URL.url()}resultsWithID.pl?run_id=<%=params[:run_id]%>"
 
+  div('results', :id => "table")
   expected_element(:results, 60)
+
+  p('job_id', :id => 'job_id')
+  p('precursors_count', :id => 'precursors_count')
+
+  a('select_all', :id => 'select-all')
+  a('deselect_all', :id => 'deselect-all')
+
+  radio_button('fasta', :id => 'export-fas')
+
+  button('export', :id => 'export-button')
 
 end
