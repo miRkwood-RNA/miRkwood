5e3920525c50b79a940dc1fdd4151acb2e423ff6
i-guigon <isabelleguigon86@gmail.com>
2014-12-15 16:51:01 +0000

    miRkwood: reads clouds for known miRNAs are stored in reads/known.

--- cgi-bin/web_scripts/BAMresults.pl
+++ cgi-bin/web_scripts/BAMresults.pl
@@ -22,7 +22,7 @@ my @js  = (miRkwood::WebTemplate->get_js_file());
 
 
 ##### Parameters
-my $cgi = new CGI;
+my $cgi = CGI->new;
 my $id_job = $cgi->param('run_id');    # get id job
 
 my $genome_file;
@@ -33,54 +33,54 @@ my $mirbase_file;
 
 ##### Create page
 my $valid = miRkwood::Results->is_valid_jobID($id_job);
-my $html = "";
-my $HTML_additional = "";
-my $page = "";
+my $html = '';
+my $HTML_additional = '';
+my $page = '';
 
-$HTML_additional .= "<p class='header-results' id='job_id'><b>Job ID:</b> " . $id_job . '</p>';
+$HTML_additional .= '<p class="header-results" id="job_id"><b>Job ID:</b> ' . $id_job . '</p>';
 
 if ( $valid ){
-    
+
     my $absolute_job_dir = miRkwood::Results->jobId_to_jobPath($id_job);
     #~ my $cfg = File::Spec->catfile( $absolute_job_dir, 'run_options.cfg' );
     #~ my $cfg = miRkwood->CONFIG();
     #~ 
     #~ my $species = $cfg->param('job.plant');
-    my $species = "Arabidopsis_thaliana";
-    if ( $species ne "" ){
-        $genome_file = File::Spec->catfile( miRkwood::Paths->get_data_path(), "genomes/", $species . ".fa");
-        $mirbase_file = File::Spec->catfile( miRkwood::Paths->get_data_path(), "miRBase/", $species . "_miRBase.gff3");
+    my $species = 'Arabidopsis_thaliana';
+    if ( $species ne '' ){
+        $genome_file = File::Spec->catfile( miRkwood::Paths->get_data_path(), "genomes/$species.fa");
+        $mirbase_file = File::Spec->catfile( miRkwood::Paths->get_data_path(), "miRBase/${species}_miRBase.gff3");
     }
     else{
-        $genome_file = $absolute_job_dir . "/genome.fa";
+        $genome_file = "$absolute_job_dir/genome.fa";
     }    
-    
-    my $known_mirnas = "";
-    my $mirna_bed = "";
-    my $other_bed = "";
-    my $final_bed = "";
+
+    my $known_mirnas = '';
+    my $mirna_bed    = '';
+    my $other_bed    = '';
+    my $final_bed    = '';
     
     opendir (my $dh, $absolute_job_dir) or die "Cannot open $absolute_job_dir : $!";
     while (readdir $dh) {
         if ( /_miRNAs.bed/ ){
             $mirna_bed = File::Spec->catfile($absolute_job_dir, $_);
         }
-        elsif ( /_other.bed/ ){ 
+        elsif ( /_other.bed/ ){
             $other_bed = File::Spec->catfile($absolute_job_dir, $_);
         }
-        elsif ( /_filtered.bed/ ){ 
+        elsif ( /_filtered.bed/ ){
             $final_bed = File::Spec->catfile($absolute_job_dir, $_);
-        }        
+        }
     }
     closedir $dh;
     
-    if ( $mirna_bed ne "" ){
-        $known_mirnas = "<div id='table'>";
-        $known_mirnas .= miRkwood::Results->known_mirnas_for_jobID($mirna_bed, $mirbase_file, $genome_file, "$absolute_job_dir/reads/" );
-        $known_mirnas .= "</div>";
+    if ( $mirna_bed ne '' ){
+        $known_mirnas  = '<div id="table">';
+        $known_mirnas .= miRkwood::Results->known_mirnas_for_jobID($mirna_bed, $mirbase_file, $genome_file, "$absolute_job_dir/reads/known" );
+        $known_mirnas .= '</div>';
     }
-    
-    
+
+
     $page = <<"END_TXT";
 <body>
     <div class="theme-border"></div>
@@ -98,7 +98,7 @@ if ( $valid ){
 </body>    
     
 END_TXT
-    
+
     $html = miRkwood::WebTemplate::get_HTML_page_for_body($page, \@css, \@js);
 }
 else{
@@ -109,7 +109,7 @@ else{
 </div><!-- main -->
 END_TXT
 
-	$html = miRkwood::WebTemplate::get_HTML_page_for_content( $page, \@css, \@js, 1 );    
+	$html = miRkwood::WebTemplate::get_HTML_page_for_content( $page, \@css, \@js, 1 );
 }
 
 
