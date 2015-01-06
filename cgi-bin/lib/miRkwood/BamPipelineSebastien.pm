43f771ecbca9afb1846d6b4bb66119be16a5051a
i-guigon <isabelleguigon86@gmail.com>
2015-01-06 13:42:00 +0000

    miRkwood: Replace "bam_file" by "bed_file" in the attributes of BamPipelineSebastien.

--- cgi-bin/lib/miRkwood/BamPipelineSebastien.pm
+++ cgi-bin/lib/miRkwood/BamPipelineSebastien.pm
@@ -19,10 +19,10 @@ Constructor
 
 sub new {
     my ( $class, @args ) = @_;
-    my ($job_dir, $bam_file, $genome_file) = @args;
+    my ($job_dir, $bed_file, $genome_file) = @args;
     my $self = {
         job_dir => $job_dir,
-        bam_file => $bam_file,
+        bed_file => $bed_file,
         genome_file => $genome_file,
         sequences => undef
     };
@@ -38,7 +38,7 @@ sub init_sequences {
     my ($self, @args) = @_;
     debug( "Extracting sequences from genome using BAM clusters", miRkwood->DEBUG() );
     my $clustering = miRkwood::ClustersSebastien->new($self->{'genome_file'});
-    my $reads = $clustering->get_read_distribution_from_bam($self->{'bam_file'});
+    my $reads = $clustering->get_read_distribution_from_bed($self->{'bed_file'});
     my $sequences = $clustering->get_windows($reads, 2);
     $self->{'sequences'} = $sequences;
     $self->{'clustering'} = $clustering;
