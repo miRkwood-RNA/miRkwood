715d9d840252d8ddb86afa245f4088d9fc584b0d
i-guigon <isabelleguigon86@gmail.com>
2015-01-12 15:18:16 +0000

    miRkwood: Add some verif (good results directory, external programs well installed).

--- cgi-bin/web_scripts/BAMpipeline.pl
+++ cgi-bin/web_scripts/BAMpipeline.pl
@@ -31,8 +31,31 @@ my $log_file = File::Spec->catfile( $absolute_job_dir, 'log.log' );
 local $Log::Message::Simple::DEBUG_FH = miRkwood->LOGFH($log_file);
 
 
+##### Check results directory
+my $root = miRkwood::Paths->get_results_filesystem_path();
+
+if (! -e $root) {
+    my $error = "Designated directory ($root) for results does not exist. Please contact the system administrator";
+    miRkwood::WebTemplate::web_die($error);
+}
+
+if (! -W $root) {
+    my $error = "Cannot write results in designated directory $root. Please contact the system administrator";
+    miRkwood::WebTemplate::web_die($error);
+}
+
+
+##### Check external softwares
+miRkwood::Programs::init_programs();
+my @unavailable = miRkwood::Programs::list_unavailable_programs();
+if (@unavailable){
+    my $error = "Cannot find required third-party software: @unavailable. Please contact the system administrator";
+    miRkwood::WebTemplate::web_die($error);
+}
+
+
 ##### Get parameters
-my $cgi = new CGI;
+my $cgi        = CGI->new();
 my $job_title  = $cgi->param('job');
 my $mail       = $cgi->param('mail');
 my $species    = $cgi->param('species');
@@ -53,55 +76,57 @@ if ( $randfold   ) { $randfold   = 1 } else { $randfold   = 0 }
 if ( $align      ) { $align      = 1 } else { $align      = 0 }
 if ( !$job_title ) { $job_title  = 0 }
 
-# Download BED file, check it and write it in the results directory
-my $bedFile = "";
-$bedFile   = $cgi->upload("bedFile") or miRkwood::WebTemplate::web_die("Error when getting BED file: $!");
-my $localBED = $absolute_job_dir . "/" . $cgi->param('bedFile');
-open (BED, ">$localBED") or miRkwood::WebTemplate::web_die("Error when creating BED file: $!");
+
+##### Download BED file, check it and write it in the results directory
+my $bedFile = '';
+$bedFile   = $cgi->upload('bedFile') or miRkwood::WebTemplate::web_die("Error when getting BED file: $!");
+my $localBED = $absolute_job_dir . '/' . $cgi->param('bedFile');
+open (my $BED, '>', $localBED) or miRkwood::WebTemplate::web_die("Error when creating BED file: $!");
 while ( <$bedFile> ){
-    print BED $_;
+    print $BED $_;
     if ( ! miRkwood::Utils::is_correct_BED_line($_) ){
-        print $cgi->redirect($error_url . "?type=noBED");
+        print $cgi->redirect($error_url . '?type=noBED');
         exit;        
     }
 }
-close BED;
+close $BED;
 
-# Get species or reference sequence
+
+##### Get species or reference sequence
 my $seqArea = $cgi->param('seqArea');
-my $genome = "";
+my $genome = '';
 my $max_length = 100000;
 if ( $seqArea eq q{} )    # case model organism
 {
-    debug("Reference species is a model organism.", 1);
-    $genome = File::Spec->catfile( miRkwood::Paths->get_data_path(), "genomes/", $species . ".fa");
+    debug('Reference species is a model organism.', 1);
+    $genome = File::Spec->catfile( miRkwood::Paths->get_data_path(), 'genomes/', $species . '.fa');
 }
 else{
-    debug("Reference sequence is provided by the user.", 1);
+    debug('Reference sequence is provided by the user.', 1);
     
     # Check if genome is a valid fasta   
     $seqArea = miRkwood::Utils::cleanup_fasta_sequence($seqArea);
     
     if ( ! miRkwood::Utils::is_fasta($seqArea) )
     {
-        print $cgi->redirect($error_url . "?type=noFasta");
+        print $cgi->redirect($error_url . '?type=noFasta');
         exit;
     }    
     if ( ! miRkwood::Utils::check_nb_sequences($seqArea) )
     {
-        print $cgi->redirect($error_url . "?type=severalSequences");
+        print $cgi->redirect($error_url . '?type=severalSequences');
         exit;
     }    
     if ( ! miRkwood::Utils::check_sequence_length($seqArea, $max_length) )
     {
-        print $cgi->redirect($error_url . "?type=tooLongSequence");
+        print $cgi->redirect($error_url . '?type=tooLongSequence');
         exit;
     }
 
-    $genome = $absolute_job_dir . "/genome.fa";
-    open (GENOME, ">$genome") or miRkwood::WebTemplate::web_die("Error when creating genome file: $!");
-    print GENOME $seqArea;
-    close GENOME;    
+    $genome = $absolute_job_dir . '/genome.fa';
+    open (my $GENOME, '>', $genome) or miRkwood::WebTemplate::web_die("Error when creating genome file: $!");
+    print $GENOME $seqArea;
+    close $GENOME;    
        
 }
 
@@ -116,7 +141,7 @@ print $cgi->redirect( $waiting_url );
 ##### Create config file
 my $run_options_file = miRkwood::Paths->get_job_config_path($absolute_job_dir);
 miRkwood->CONFIG_FILE($run_options_file);
-miRkwood::write_config_for_bam_pipeline( $run_options_file, $job_title, $species, 'BAM', $align, $species_db, $filter_CDS, $filter_tRNA_rRNA, $filter_multimapped, $mfei, $randfold);
+miRkwood::write_config_for_bam_pipeline( $run_options_file, $job_title, $species, 'WebBAM', $align, $species_db, $filter_CDS, $filter_tRNA_rRNA, $filter_multimapped, $mfei, $randfold);
 
 
 ##### Filter BED
