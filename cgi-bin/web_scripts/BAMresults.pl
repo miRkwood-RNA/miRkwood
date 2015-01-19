95cfd5eac017e403b7ef97fbf7ed0f2213e6b7b2
i-guigon <isabelleguigon86@gmail.com>
2015-01-19 10:23:24 +0000

    miRkwood: Add a summary of chosen options on the results page.
    The links to download the BED files are currently not working.

--- cgi-bin/web_scripts/BAMresults.pl
+++ cgi-bin/web_scripts/BAMresults.pl
@@ -45,6 +45,7 @@ my $id_job = $cgi->param('run_id');    # get id job
 my $valid = miRkwood::Results->is_valid_jobID($id_job);
 my $html = '';
 my $HTML_additional = '';
+my $HTML_results = '';
 my $page = '';
 
 my $mirna_bed;
@@ -63,7 +64,7 @@ if ( $valid ){
     my $run_options_file = miRkwood::Paths->get_job_config_path($absolute_job_dir);
     miRkwood->CONFIG_FILE($run_options_file);
     my $cfg = miRkwood->CONFIG();
-    
+
     opendir (my $dh, $absolute_job_dir) or die "Cannot open $absolute_job_dir : $!";
     while (readdir $dh) {
         if ( /_miRNAs.bed/ ){
@@ -71,7 +72,7 @@ if ( $valid ){
         }
         elsif ( /_filtered.bed/ ){
             $final_bed = File::Spec->catfile($absolute_job_dir, $_);
-        }        
+        }
         elsif ( /_other.bed/ ){
             $other_bed = File::Spec->catfile($absolute_job_dir, $_);
         }
@@ -83,7 +84,7 @@ if ( $valid ){
         }
         elsif ( /.bed/ ){
             $initial_bed = File::Spec->catfile($absolute_job_dir, $_);
-        }        
+        }
     }
     closedir $dh;    
 
@@ -101,6 +102,72 @@ if ( $valid ){
 	unless ( miRkwood::Results->is_job_finished($id_job) ) {
 		$HTML_additional .= "<p class='warning'>Still processing...</p>";
 	} else {
+        ##### Summary of options
+        my $basename_bed = '';
+        if ( $initial_bed =~ /.*(\/|\\)([^\/]+)/ ){
+            $basename_bed = $2;
+        }
+        $HTML_additional .= "<div class='results_summary'><ul>";
+        $HTML_additional .= '<h2>Options summary:</h2>';
+        $HTML_additional .= '<br />';
+        $HTML_additional .= "<li>BED file: $basename_bed</li>";
+
+        # Reference species
+        if ( $cfg->param('job.plant') ){
+            $HTML_additional .= '<li>Reference species: ' . $cfg->param('job.plant') . '</li>';
+        }
+
+        # Align
+        if ( $cfg->param('options.align') ){
+            $HTML_additional .= "<li>Flag conserved mature miRNAs: yes</li>";
+        }
+        else{
+            $HTML_additional .= "<li>Flag conserved mature miRNAs: no</li>";
+        }
+
+        # MFEI
+        if ( $cfg->param('options.mfei') ){
+            $HTML_additional .= "<li>Select only sequences with MFEI < -0.6: yes</li>";
+        }
+        else{
+            $HTML_additional .= "<li>Select only sequences with MFEI < -0.6: no</li>";
+        }
+
+        # Ranfold
+        if ( $cfg->param('options.ranfold') ){
+            $HTML_additional .= "<li>Compute thermodynamic stability: yes</li>";
+        }
+        else{
+            $HTML_additional .= "<li>Compute thermodynamic stability: no</li>";
+        }                
+
+        # CDS
+        if ( $cfg->param('options.filter_CDS') ){
+            $HTML_additional .= "<li>Filter CoDing Sequences: yes</li>";
+        }
+        else{
+            $HTML_additional .= "<li>Filter CoDing Sequences: no</li>";
+        }
+
+        # tRNA and rRNA
+        if ( $cfg->param('options.filter_tRNA_rRNA') ){
+            $HTML_additional .= "<li>Filter tRNA and rRNA: yes</li>";
+        }
+        else{
+            $HTML_additional .= "<li>Filter tRNA and rRNA: no</li>";
+        }
+
+        # Multimapped reads
+        if ( $cfg->param('options.filter_multimapped') ){
+            $HTML_additional .= "<li>Filter multimapped reads: yes</li>";
+        }
+        else{
+            $HTML_additional .= "<li>Filter multimapped reads: no</li>";
+        }
+
+        $HTML_additional .= "</ul></div>";
+
+        ##### Summary of results
         $nb_new_results   = miRkwood::Results->number_of_results_bis( $id_job, 'new' );
         $nb_known_results = miRkwood::Results->number_of_results_bis( $id_job, 'known' );
         $nb_total_reads   = miRkwood::BEDHandler::count_reads_in_bed_file( $initial_bed );
@@ -109,29 +176,29 @@ if ( $valid ){
         my $known_url = miRkwood::WebTemplate::get_cgi_url('BAMresults_for_mirnas.pl') . $arguments . '&type=known';
         my $new_url = miRkwood::WebTemplate::get_cgi_url('BAMresults_for_mirnas.pl') . $arguments . '&type=new';
 
-        $HTML_additional .= "<ul>";
-        $HTML_additional .= "<li>Total number of reads: $nb_total_reads (XXX unique reads)</li>";
+        $HTML_results .= "<div class='results_summary'><ul>";
+        $HTML_results .= '<h2>Results summary:</h2>';
+        $HTML_results .= '<br />';
+        $HTML_results .= "<li>Total number of reads: $nb_total_reads (XXX unique reads)</li>";
 
         if ( -r $cds_bed ){
             $nb_CDS_reads     = miRkwood::BEDHandler::count_reads_in_bed_file( $cds_bed );
-            $HTML_additional .= "<li>CoDing Sequences: $nb_CDS_reads reads (download)</li>";            
+            $HTML_results .= "<li>CoDing Sequences: $nb_CDS_reads reads (<a href=$cds_bed>download</a>)</li>";            
         }
         if ( -r $nb_other_reads ){
             $nb_other_reads   = miRkwood::BEDHandler::count_reads_in_bed_file( $other_bed );
-            $HTML_additional .= "<li>rRNA/tRNA: $nb_other_reads reads (download)</li>";
+            $HTML_results .= "<li>rRNA/tRNA: $nb_other_reads reads (<a href=$other_bed>download</a>)</li>";
         }
         if ( -r $nb_multi_reads ){
             $nb_multi_reads   = miRkwood::BEDHandler::count_reads_in_bed_file( $multimapped_bed ); 
-            $HTML_additional .= "<li>Frequent reads: $nb_multi_reads reads (download)</li>";
+            $HTML_results .= "<li>Frequent reads: $nb_multi_reads reads (<a href=$multimapped_bed>download</a>)</li>";
         }
 
-        $HTML_additional .= "<li>Known miRNAs: $nb_known_results sequence(s) (<a href=$known_url>see results</a>)</li>";
-        $HTML_additional .= "<li>Novel miRNAs: $nb_new_results sequence(s) (<a href=$new_url>see results</a>)</li>";
-        $HTML_additional .= "</ul>";
+        $HTML_results .= "<li>Known miRNAs: $nb_known_results sequence(s) (<a href=$known_url>see results</a>)</li>";
+        $HTML_results .= "<li>Novel miRNAs: $nb_new_results sequence(s) (<a href=$new_url>see results</a>)</li>";
+        $HTML_results .= "</ul></div>";
 
     }
-
-    #~ $HTML_additional .= "</div>";
 
 
     $page = <<"END_TXT";
@@ -144,6 +211,8 @@ if ( $valid ){
         $header_menu
         <div class="main main-full">
             $HTML_additional
+            
+            $HTML_results
         </div><!-- main -->
     </div><!-- bloc droit-->
     $footer
