856a5d3a3dfeedebe23cc435c64464bcb2f06a0b
i-guigon <isabelleguigon86@gmail.com>
2015-01-28 10:45:26 +0000

    miRkwood: Now the page id.php calls the page wait.pl which determines the pipeline used and call the correct results page in consequence.

--- html/id.php
+++ html/id.php
@@ -1,40 +1,51 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
+    <head>
         <link type='text/css' rel='stylesheet' href='../Style/bioinfo.css' />
 		<link type='text/css' rel='stylesheet' href='./style/script.css' />
 		<link type='text/css' rel='stylesheet' href='./style/rna.css' />
         <script type='text/javascript' src='/js/miARN.js'></script>
         <title>miRkwood - MicroRNA identification</title>
     </head>
- <body>
-<div class="theme-border"></div>
+    
+    <body>
+        <div class="theme-border"></div>
+        
         <a href="/">
             <div class="logo"></div>
         </a>
-         <? include("./static/bioinfo_menu.txt") ?>
+            
+        <? include("./static/bioinfo_menu.txt") ?>
+        
         <div class="bloc_droit">
-        <? include("./static/header_menu.txt") ?>
-<div class="main">
+            
+            <? include("./static/header_menu.txt") ?>
+            
+            <div class="main">
+
+                <br/>
+                <h2>Retrieve result with an ID</h2>
+                <br/>
+  
+  
+                <div class="forms">
+                    <form method="post" action="/cgi-bin/mirkwood/web_scripts/wait.pl">
+                        <p>The ID remains valid 15 days after sequence submission.</p>
+                        <p>
+                            <label for='jobId'><b>Enter the ID:</b></label>
+                            <input type="text" name="jobId" id='jobId' size="20"/>
+                            <input type="hidden" name="command" value="result"/>
+                            <input type="submit" value="Go"/>
+                        </p>
+                    </form>
+                </div> <!-- form -->
 
 
-  <br/>
-  <h2>Retrieve result with an ID</h2>
-  <br/>
-  <div class="forms">
-    <form method="post" action="/cgi-bin/mirkwood/web_scripts/resultsWithID.pl">
-      <p>The ID remains valid 15 days after sequence submission.</p>
-      <p>
-      <label for='run_id'><b>Enter the ID:</b></label>
-        <input type="text" name="run_id" id='run_id' size="20"/>
-        <input type="hidden" name="command" value="result"/>
-        <input type="submit" value="Go"/>
-      </p>
-    </form>
-  </div> <!-- form -->
- </div> <!-- main -->
+            </div> <!-- main -->
         </div><!-- bloc droit-->
-       <? include("./static/footer.txt") ?>
+        
+        <? include("./static/footer.txt") ?>
+       
     </body>
     
 </html>
