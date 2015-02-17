7e8139d35d4ac9d5e207cdd0666af9b02ee75e16
i-guigon <isabelleguigon86@gmail.com>
2015-02-17 17:06:31 +0000

    miRkwood: Delete the field 'alignment' in optional fields in case of known miRNAs.

--- cgi-bin/lib/miRkwood/Results.pm
+++ cgi-bin/lib/miRkwood/Results.pm
@@ -175,6 +175,7 @@ sub convert_basic_to_pseudoXML {
 
     if ( $type eq 'Known' ){
         push @headers, 'precursor_name';
+        @optional_fields = miRkwood::Utils::delete_element_in_array( 'alignment', @optional_fields );
     }
 
     push @headers, ( 'name', 'position', 'length', 'strand', 'quality', @optional_fields, 'image', 'identifier' ); 
