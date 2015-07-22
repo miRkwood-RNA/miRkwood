77f2a6df188bea6727682c36a1ee673d71ac47fe
i-guigon <isabelleguigon86@gmail.com>
2015-07-22 14:59:54 +0000

    miRkwood: Completely new version of mirkwood-bam2bed.pl:
    - accept a BAM or a SAM in input
    - the other argument is the BED file instead of a directory.
    Temporary files are created in /tmp directory (and then
    they are deleted, of course).
    - fix a bug about strand when when we have several reads
    on the same positions and on the two strands (previouly
    one strand were lost).

--- cgi-bin/scripts/mirkwood-bam2bed.pl
+++ cgi-bin/scripts/mirkwood-bam2bed.pl
@@ -16,120 +16,121 @@ use Getopt::Long;
 
 
 ########## Variables
-my $bam_file = '';
-my $sam_file;
-my $bed_file;
-my $output_directory;
-my $tmp_file;
+my $input_file = '';
+my $bed_file   = '';
+my $time = time();
+my $sorted_bam_file = "/tmp/mirkwood_bam2bed_${time}_sorted";
+my $sorted_sam_file = "/tmp/mirkwood_bam2bed_${time}_sorted.sam";
+
 my $counts;
-my $names;
-my $sequence;
-my $id;
-my $chromosome;
-my $start;
-my $end;
 my $help;
-my $help_message = "mirkwood-bam2bed.pl
+my $help_message = <<'EOF';
+mirkwood-bam2bed.pl
 ----------
 Script to convert a BAM into a BED file for use by miRkwood.
 
-Usage : ./mirkwood-bam2bed.pl -bam <input BAM file> -out <output directory> 
+Usage : ./mirkwood-bam2bed.pl -bam <input BAM file> -bed <output BED file> 
 
 Dependancies : samtools
-Make sure to have it installed in your PATH. For Ubuntu/Debian distributions `sudo apt-get install samtools` is enough.\n";
+Make sure to have it installed in your PATH. For Ubuntu/Debian distributions `sudo apt-get install samtools` is enough.
+
+EOF
 
 
 ########## Get options
-GetOptions ('bam=s' => \$bam_file,
-            'out=s' => \$output_directory,
+GetOptions ('in=s'  => \$input_file,
+            'bed=s' => \$bed_file,
 	        'help'  => \$help);
 
 
 ########## Validate options
-if ( $help or ! -r $bam_file or ! $output_directory ){
+if ( $help ){
     print $help_message;
     exit;
 }
 
-if (! -d $output_directory){
-	mkdir $output_directory, 0777;
+if ( ! -r $input_file ){
+    print "Missing input file!\n";
+    print $help_message;
+    exit;
+}
+
+if ( $bed_file eq '' ){
+    print "Missing output file!\n";
+    print $help_message;
+    exit;
 }
 
 
-########## Convert BAM into SAM and filtering out unmapped reads
-if ( $bam_file =~ /([^\/\\]+)[.]bam/ ){
-    $sam_file = "$output_directory/$1.sam";
-    $bed_file = "$output_directory/$1.bed";
-    $tmp_file = "$output_directory/$1_sorted";
+
+########## Create sorted SAM file
+if ( $input_file =~ /([^\/\\]+)[.]sam/ ){
+    system("sort -k 3,3 -k 4,4n $input_file | grep -v \"^@\" > $sorted_sam_file");
+}
+elsif ( $input_file =~ /([^\/\\]+)[.]bam/ ){
+    ##### Sort BAM file
+    system("samtools sort $input_file $sorted_bam_file");
+
+    ##### Convert sorted BAM into SAM and filter out unmapped reads
+    system("samtools view -F 4 $sorted_bam_file.bam > $sorted_sam_file");
+    unlink $sorted_bam_file . '.bam';
 }
 else{
-    die "Non correct input file.\n$help_message";
+    die "Non correct input file. We accept BAM and SAM as input formats.\n$help_message";
 }
 
-system("samtools sort $bam_file $tmp_file");
-system("samtools view -h -F 4 $tmp_file.bam > $sam_file");
 
 
 ########## Read the SAM file to store counts into a hash
-open(my $SAM, '<', $sam_file) or die "ERROR while reading SAM file. Program will end prematurely.\n";
+open(my $SAM, '<', $sorted_sam_file) or die "ERROR while reading SAM file. Program will end prematurely.\n";
 
 while ( <$SAM> ){
 
-    if ( ! /^@/ ){
+    chomp;
 
-        chomp;
+    my @line = split ( /\t/smx );
 
-        my @line = split ( /\t/smx );
+    if ( $line[1] ne '0x4' && $line[1] ne '4' ){
 
-        $id = $line[0];
-        $chromosome = $line[2];
-        $start = $line[3] - 1;
-        $sequence = $line[9];
-
-        if ( ! exists( $counts->{$chromosome}{$start}{$sequence} ) ){
-            $counts->{$chromosome}{$start}{$sequence}{'count'} = 0;
-        }
-        $counts->{$chromosome}{$start}{$sequence}{'count'}++;
-        $counts->{$chromosome}{$start}{$sequence}{'strand'} = '+';
+        my $id = $line[0];
+        my $chromosome = $line[2];
+        my $start = $line[3] - 1;
+        my $sequence = $line[9];
+        my $strand = '+';
         if ( $line[1] eq '16' or $line[1] eq '0x10' ){
-            $counts->{$chromosome}{$start}{$sequence}{'strand'} = '-';
+            $strand = '-';
         }
 
-        if ( ! exists( $names->{$sequence} ) ){
-            $names->{$sequence} = $id;
+        if ( ! exists( $counts->{$chromosome}{$start}{$sequence}{$strand} ) ){
+            $counts->{$chromosome}{$start}{$sequence}{$strand}{'count'} = 0;
         }
+        $counts->{$chromosome}{$start}{$sequence}{$strand}{'count'}++;
+
+        $counts->{$chromosome}{$start}{$sequence}{$strand}{'id'} = $id;
 
     }
 
 }
 
 close $SAM;
+unlink $sorted_sam_file;
 
 
 ########## Browse hash tables and print data in BED file
 open(my $BED, '>', $bed_file) or die "ERROR while creating $bed_file. Program will end prematurely.\n";
-
-foreach $chromosome ( sort (keys%$counts) ){
-
-    foreach $start ( sort {$a <=> $b} keys%{$counts->{$chromosome}} ){
-
-        foreach $sequence (sort (keys%{$counts->{$chromosome}{$start}}) ){
-            $end = $start + length($sequence);
-
-            print $BED "$chromosome\t";
-            print $BED "$start\t";
-            print $BED "$end\t";
-            print $BED "$names->{$sequence}\t";
-            print $BED "$counts->{$chromosome}{$start}{$sequence}{'count'}\t";
-            print $BED "$counts->{$chromosome}{$start}{$sequence}{'strand'}\n";
+foreach my $chromosome ( sort (keys%$counts) ){
+    foreach my $start ( sort {$a <=> $b} keys%{ $counts->{$chromosome} } ){
+        foreach my $sequence ( sort (keys%{ $counts->{$chromosome}{$start} } ) ){
+            foreach my $strand ( sort (keys%{ $counts->{$chromosome}{$start}{$sequence} } ) ){ 
+                my $end = $start + length($sequence);
+                print $BED "$chromosome\t";
+                print $BED "$start\t";
+                print $BED "$end\t";
+                print $BED "$counts->{$chromosome}{$start}{$sequence}{$strand}{'id'}\t";
+                print $BED "$counts->{$chromosome}{$start}{$sequence}{$strand}{'count'}\t";
+                print $BED "$strand\n";
+            }
         }
-
     }
-
 }
-
 close $BED;
-
-
-unlink "$tmp_file.bam";
-unlink $sam_file;
