02cb482271f23a6976cf72f543f0232284c9fac3
i-guigon <isabelleguigon86@gmail.com>
2015-08-04 12:03:34 +0000

    miRkwood: Add a checking to avoid a warning.

--- cgi-bin/lib/miRkwood/ResultsExporter/ResultsExporter.pm
+++ cgi-bin/lib/miRkwood/ResultsExporter/ResultsExporter.pm
@@ -50,7 +50,7 @@ sub get_file_extension {
 
 sub get_type {
     my ( $self, @args ) = @_;
-    if ( $self->{'mirna_type'} ne '' ) {
+    if ( defined($self->{'mirna_type'}) && $self->{'mirna_type'} ne '' ) {
         return "_$self->{'mirna_type'}";
     }    
     else {
