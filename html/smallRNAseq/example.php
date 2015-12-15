54e911b50c181ca16c04bdc1372e5f7a7beeb4cb
i-guigon <isabelleguigon86@gmail.com>
2015-12-15 09:42:12 +0000

    miRkwood: change the order the css files are called.

--- html/smallRNAseq/example.php
+++ html/smallRNAseq/example.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
-        <link type='text/css' rel='stylesheet' href='/Style/css/bioinfo.css' />
         <link type='text/css' rel='stylesheet' href='../style/mirkwood.css' />
         <link href="/Style/css/page_theme.css" rel="stylesheet" style="text/css"/>
         <link type='text/css' rel='stylesheet' href='../style/help.css' />
         <link type='text/css' rel='stylesheet' href='../style/rna.css' />
+        <link type='text/css' rel='stylesheet' href='/Style/css/bioinfo.css' />
         <script type="text/javascript" src="../../libs/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="../../libs/jquery.history.js"></script>
         <script type="text/javascript" src="../js/header.js"></script>
--- html/smallRNAseq/index.php
+++ html/smallRNAseq/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
-        <link type='text/css' rel='stylesheet' href='../../Style/css/bioinfo.css' />
         <link type='text/css' rel='stylesheet' href='../style/mirkwood.css' />
+        <link type='text/css' rel='stylesheet' href='../../Style/css/bioinfo.css' />
         <script type="text/javascript" src="../../libs/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="../../libs/jquery.history.js"></script>
         <script type="text/javascript" src="../js/header.js"></script>
