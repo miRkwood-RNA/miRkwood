699e407734455c655ae4523dae1ec8f4c6267ce4
i-guigon <isabelleguigon86@gmail.com>
2016-01-13 14:49:34 +0000

    miRkwood: Don't call imgpreview.full.jquery.js, it's not used here.

--- cgi-bin/web_scripts/BAMresults.pl
+++ cgi-bin/web_scripts/BAMresults.pl
@@ -31,8 +31,7 @@ my @js = (
 	File::Spec->catfile( miRkwood::WebPaths->get_js_path(), 'results.js' ),
 	File::Spec->catfile( miRkwood::WebPaths->get_js_path(), 'graphics.js' ),
 	File::Spec->catfile( miRkwood::WebPaths->get_js_path(), 'miARN.js' ),
-	File::Spec->catfile( miRkwood::WebPaths->get_js_path(), 'jquery.min.js' ),
-	File::Spec->catfile( miRkwood::WebPaths->get_js_path(), 'imgpreview.full.jquery.js' ),
+	File::Spec->catfile( miRkwood::WebPaths->get_js_path(), 'jquery.min.js' )
     miRkwood::WebTemplate->get_bioinfo_js_file()
 );
 
