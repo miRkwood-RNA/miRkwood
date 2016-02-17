24b10e7159dba33e00793dc95881dd5cc527dbc0
i-guigon <isabelleguigon86@gmail.com>
2016-02-17 14:46:49 +0000

    miRkwood: smallRNAseq pipeline, Web version: now we can export the results in org-mode format.

--- cgi-bin/web_scripts/BAMresults_for_mirnas.pl
+++ cgi-bin/web_scripts/BAMresults_for_mirnas.pl
@@ -111,6 +111,7 @@ if ( $valid ){
                         <input type="radio" name="export" id="export-fas" value="fas" />&#160;<label for='export-fas'>FASTA format</label><br/>
                         <input type="radio" name="export" id="export-dot" value="dot" />&#160;<label for='export-dot'>dot-bracket format (plain sequence + secondary structure)</label><br/>
                         <input type="radio" name="export" id="export-odf" value="odf" />&#160;<label for='export-odf'>full report in document format (ODF)</label><br/>
+                        <input type="radio" name="export" id="export-org" value="org" />&#160;<label for='export-org'>full report in ORG-mode format</label><br/>
                         <input type="radio" name="export" id="export-gff" value="gff" />&#160;<label for='export-gff'>GFF format</label><br/>
                         <input type="radio" name="export" id="export-reads" value="reads" />&#160;<label for='export-reads'>read cloud format</label>
                         <input style="margin-left:360px" class="myButton" type="button" name="export-button" id='export-button' value="Export" onclick='exportTo("$id_job", "$web_scripts", "smallRNAseq", "$mirnas_type")'/>
