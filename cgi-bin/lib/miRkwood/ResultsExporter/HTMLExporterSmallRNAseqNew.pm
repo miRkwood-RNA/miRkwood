da3d6c242736dae2e8770b72925461ae9a76bce6
i-guigon <isabelleguigon86@gmail.com>
2016-10-14 14:49:05 +0000

    miRkwood: HTML results page (CLI smallRNAseq pipeline, new candidates): change the header.
    Now we have a part for the candidate features (chromosome, position,
    strand, nb of reads, reads distribution score, mfei and optionally 
    shuffles) and a part for the mirna features(sequence, length, weight
    and optionally alignment).
    miRNA features have an additional header line (with just "miRNA")
    while candidate features are in cells that spread on 2 lines.

--- cgi-bin/lib/miRkwood/ResultsExporter/HTMLExporterSmallRNAseqNew.pm
+++ cgi-bin/lib/miRkwood/ResultsExporter/HTMLExporterSmallRNAseqNew.pm
@@ -30,31 +30,52 @@ sub get_headers {
 
 sub get_header {
     my ( $self, @args ) = @_;
-    my $output = '<tr>';
     my $left_border_cell = "class='left_border'";
+    my @optional_candidate_fields = miRkwood::Candidate->get_optional_candidate_fields();
+    my @optional_mirna_fields = miRkwood::Candidate->get_optional_mirna_fields();
+    my $nb_mirna_fields = scalar( @optional_mirna_fields ) + 3;
+
+    my $output = '<tr>';
+
     for my $header ( ('chromosome'), $self->get_headers() ) {
+        # candidate fields
         if ( $header eq 'chromosome' ){
-            $output .= "<th>chr</th>\n";
+            $output .= "<th rowspan='2'>chr</th>\n";
         }
+        elsif ( $header eq 'position' ){
+            $output .= "<th rowspan='2'>position</th>\n";
+        }
+        elsif ( $header eq 'strand' ){
+            $output .= "<th rowspan='2'>strand</th>\n";
+        }
+        elsif ( $header eq 'nb_reads' ){
+            $output .= "<th rowspan='2'>reads</th>\n";
+        }
+        elsif ( $header eq 'reads_distribution' ){
+            $output .= "<th rowspan='2'>reads distribution</th>\n";
+        }
+        elsif ( $header eq 'mfei' ){
+            $output .= "<th rowspan='2'>&nbsp; mfei &nbsp;</th>\n";
+        }
+        elsif ( $header eq 'shuffles' ){
+            $output .= "<th rowspan='2'>shuffles</th>\n";
+        }
+        # miRNA fields
         elsif ( $header eq 'mirna_sequence' ){
-            $output .= "<th $left_border_cell>miRNA</th>\n";
+            $output .= "<th colspan='$nb_mirna_fields' $left_border_cell>miRNA</th>\n";
+            $output .= "</tr>\n";
+            $output .= "<tr>\n";
+            $output .= "<th class='left_border'>sequence</th>\n";
         }
         elsif ( $header eq 'mirna_length' ){
             $output .= "<th>length</th>\n";
         }
-        elsif ( $header eq 'nb_reads' ){
-            $output .= "<th>reads</th>\n";
-        }
-        elsif ( $header eq 'reads_distribution' ){
-            $output .= "<th>reads distribution</th>\n";
-        }
-        elsif ( $header eq 'mfei' ){
-            $output .= "<th>&nbsp; mfei &nbsp;</th>\n";
-        }
-        else {
+        else{
             $output .= "<th>$header</th>\n";
         }
+
     }
+
     $output .= "</tr>\n";
     return $output;
 }
